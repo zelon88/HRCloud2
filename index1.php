c18fef323ef080f0ee64b9dae5658603ad5af0b8
zelon88 <zelon88@gmail.com>
2016-08-16 00:29:09 -0400

    Updated GUI
    1. Incorporated HRAI. Specifically branched/ported for HRCloud2.
    2. Finalized placement/desired functionality ergonomics.
    3. Finalized sizing/scaling for desktop index pages (I hope).
    4. Blended all the CSS, colors, and display HTML together a little.

--- index1.php
+++ index1.php
@@ -3,14 +3,16 @@
 <head>
 <title>HRCloud2 | Home </title>
 <link rel="stylesheet" type="text/css" href="style.css">
+<script type="text/javascript" src="Applications/jquery-3.1.0.min.js"></script>
 </head>
 <body>
+  <?php require 'Applications/HRAI/HRAIHelper.php'; ?>
 <div id="nav" align="center">
     <div class="nav">
       <ul>
         <li class="Cloud"><a href="index1.php">Cloud</a></li>
         <li class="Settings"><a href=""> Settings</a></li>
-        <li class="Logs"><a href="">Logging</a></li>
+        <li class="Logs"><a href="">Logs</a></li>
         <li class="Help"><a href="">Help</a></li>
       </ul>
     </div>
@@ -25,22 +27,38 @@
 
 <div id="centerdiv" align='center' style="margin: 0 auto; max-width:800px;">
 <div id="cloudCommandsDiv" style="float: left; ">
-  <iframe src="cloudCommands.php" id="cloudCommands" name="cloudCommands" width="425" height="75" scrolling="no" margin-top:-4px; margin-left:-4px; border:double; onload="document.getElementById('loading').style.display='none';">></iframe>
+  <iframe src="cloudCommands.php" id="cloudCommands" name="cloudCommands" width="415" height="105" scrolling="no" margin-top:-4px; margin-left:-4px; border:double; onload="document.getElementById('loading').style.display='none';">></iframe>
 <script type="text/javascript">
 document.getElementById("cloudCommands").submit();
 </script>
 </div>
 <div id="HRAIDiv" style="float: right; ">
-  <iframe src="Applicatiosn/HRAI/core.php" id="HRAIMini" name="HRAIMini" width="275" height="75" scrolling="no" margin-top:-4px; margin-left:-4px; border:double; onload="document.getElementById('loading').style.display='none';">></iframe>
+
+  <iframe src="Applications/HRAI/core.php" id="HRAIMini" name="HRAIMini" width="375" height="75" scrolling="yes" margin-top:-4px; margin-left:-4px; border:double; onload="document.getElementById('loading').style.display='none';">></iframe>
+  <form action="Applications/HRAI/core.php#end" id="Corefile Input" method="post" target="HRAIMini">
+  <input type="hidden" name="user_ID" value="<?php echo $user_ID;?>">
+  <input type="hidden" name="sesID" value="<?php echo $sesID;?>">
+  <input type="hidden" name="display_name" value="<?php echo $display_name;?>">
+  <?php if (!isset($input)) {
+    $input = ''; } ?>
+  <input type="text" name="input" id="input"  value="<?php echo $input; ?>">
+  <input id='submitHRAI' type="submit" value="Hello HRAI"></form></div>
 <script type="text/javascript">
-document.getElementById("cloudCommands").submit();
+document.getElementById("HRAIMini").submit();
+</script>
+<script type="text/javascript">
+jQuery('#input').on('input', function() {
+  $("#logo").attr("src","Resources/logo.gif");
+});
+</script>
+<script type="text/javascript">
+jQuery('#submitHRAI').on('submit', function() {
+  $("#logo").attr("src","Resources/logo.gif");
+});
 </script>
 </div>
-<div id="cloudContentsDiv" style="float: left; ">
-  <iframe src="cloudCore.php" id="cloudContents" name="cloudContents" width="800" height="500" scrolling="yes" margin-top:-4px; margin-left:-4px; border:double; onload="document.getElementById('loading').style.display='none';">></iframe>
-<script type="text/javascript">
-document.getElementById("cloudCoontents").submit();
-</script>
+<div id="cloudContentsDiv" style="float: center; ">
+  <iframe src="cloudCore.php" id="cloudContents" name="cloudContents" style="min-height:375px;" width="800" scrolling="yes" margin-top:-4px; margin-left:-4px; border:double; onload="document.getElementById('loading').style.display='none';">></iframe>
 </div>
 
 </body>
