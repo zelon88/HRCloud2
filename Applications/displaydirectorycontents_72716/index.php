ab805328f98c48d60436f8e9e143750a25e0c4bf
zelon88 <zelon88@gmail.com>
2016-08-22 21:19:12 -0400

    Updated GUI, Core, and Options.
    Tweaked de-archiving and archiving. 
    De-archiving via the GUI checkboxes is now possible onclick via AJAX. 
    Broke downloading capability. 
    Finalized uploading capability and GUI. 
    Uploading via the GUI is now possible. 
    Tweaked visuals. 
    Consolidated options display.

--- Applications/displaydirectorycontents_72716/index.php
+++ Applications/displaydirectorycontents_72716/index.php
@@ -14,7 +14,7 @@
    <link rel="shortcut icon" href="Applications/displaydirectorycontents_72716/favicon.ico">
    <title>Cloud Contents</title>
 </head>
-    <script type="text/javascript" src="Applications/jquery-3.1.0.min.js"></script>
+    <script type="text/javascript" src="<?php echo $URL; ?>/HRProprietary/HRCloud2/Applications/jquery-3.1.0.min.js"></script>
     <link rel="stylesheet" href="Applications/displaydirectorycontents_72716/style.css">
     <script src="Applications/displaydirectorycontents_72716/sorttable.js"></script>
     <script type="text/javascript">
@@ -25,9 +25,9 @@
        else
           e.style.display = 'block'; }
     </script>
+
 <body>
 <div id="container">
-
 	<table class="sortable">
 	    <thead>
 		<tr>
@@ -49,7 +49,6 @@ $tableCount = 0;
 		else{$size=round($size/1073741824, 1)." GB";}
 		return $size;
 	}
-
  	// Checks to see if veiwing hidden files is enabled
 	if($_SERVER['QUERY_STRING']=="hidden")
 	{$hide="";
@@ -59,45 +58,43 @@ $tableCount = 0;
 	{$hide=".";
 	 $ahref="./?hidden";
 	 $atext="Show";}
-
+$fileArray1 = array();
+?>
+<form action="cloudCore.php" method="post" enctype="multipart/form-data">
+<body><p><strong>Operations: </strong>
+<img onclick="toggle_visibility('loadingCommandDiv');" src='Resources/copy.png'/> | <img onclick="toggle_visibility('loadingCommandDiv');" src='Resources/deletesmall.png'/> | <img onclick="toggle_visibility('loadingCommandDiv');" src='Resources/archive.png'/> | <img id='dearchive' name='dearchive' onclick="toggle_visibility('loadingCommandDiv');" src='Resources/dearchive.png'/> | <img onclick="toggle_visibility('loadingCommandDiv');" src='Resources/docscan.png'/> | <img onclick="toggle_visibility('loadingCommandDiv');" src='Resources/convert.png'/> | <img onclick="toggle_visibility('loadingCommandDiv');" src='Resources/photoedit.png'/> | <img onclick="toggle_visibility('loadingCommandDiv');" src='Resources/makepdf.png'/> | <img onclick="toggle_visibility('loadingCommandDiv');" src='Resources/stream.png'/> | <img onclick="toggle_visibility('searchDiv');" src='Resources/searchsmall.png'/> | <input type="file" name="filesToUpload[]" id="filesToUpload" class="uploadbox" multiple>
+<input type='submit' name="upload" id="upload" value='&#x21E7' class="submitsmall"></p></form>
+<div align='center'><img src='Resources/logosmall.gif' id='loadingCommandDiv' name='loadingCommandDiv' style="display:none; max-width:64px; max-height:64px;"/></div>
+</div>
+<?php
 	 // Opens directory
 	 $myDirectory=opendir($CloudLoc.$UserDirPOST.$UserID);
-
 	// Gets each entry
 	while($entryName=readdir($myDirectory)) {
 	   $dirArray[]=$entryName;
 	}
-
 	// Closes directory
 	closedir($myDirectory);
-
 	// Counts elements in array
 	$indexCount=count($dirArray);
-
 	// Sorts files
 	sort($dirArray);
-
 	// Loops through the array of files
 	for($index=0; $index < $indexCount; $index++) {
-
 	// Decides if hidden files should be displayed, based on query above.
 	    if(substr("$dirArray[$index]", 0, 1)!=$hide) {
-
 	// Resets Variables
 		$favicon="";
 		$class="file";
-
 	// Gets File Names
 		$name=$dirArray[$index];
 		$namehref=$dirArray[$index];
+        $fileArray = array_push($fileArray1, $namehref);
 		if (substr_compare($namehref, '/', 1)) { 
 			$namehref = substr_replace('/'.$namehref, $namehref, 0); }
-
 	// Gets Date Modified
 		$modtime=date("M j Y g:i A", filemtime($CloudUsrDir.$dirArray[$index]));
 		$timekey=date("YmdHis", filemtime($CloudUsrDir.$dirArray[$index]));
-
-
 	// Separates directories, and performs operations on those directories
 		if(is_dir($dirArray[$index]))
 		{
@@ -105,7 +102,6 @@ $tableCount = 0;
 				$size="&lt;Directory&gt;";
 				$sizekey="0";
 				$class="dir";
-
 			// Gets favicon.ico, and displays it, only if it exists.
 				if(file_exists("$namehref/favicon.ico"))
 					{
@@ -113,7 +109,6 @@ $tableCount = 0;
 						$favicon=" style='background-image:url($slash$namehref/favicon.ico);'";
 						$extn="&lt;Website&gt;";
 					}
-
 			// Cleans up . and .. directories
 				if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;"; $favicon=" style='background-image:url($slash$namehref/favicon.ico);'";}
 				if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
@@ -185,193 +180,46 @@ if ($extn == "Folder") {
 
 	 echo("
 		<tr class='$class'>
-			<td><a id='corePostDL$tableCount' href='#'$favicon class='name' onclick=".'toggle_visibility(\'loading\');'.">$name</a></td>
-			<td><div><input type='checkbox' name='corePostSelect$tableCount' id='corePostSelect$tableCount' onclick=".'toggle_visibility(\'loading\');'."</div></td>
-            <td><a id='corePostDL$tableCount' name='corePostDL$tableCount' href='#' onclick=".'toggle_visibility(\'loading\');'.">$extn</a></td>
-			<td sorttable_customkey='$sizekey'><a id='corePostDL$tableCount' href='#' name='corePostDL$tableCount' onclick=".'toggle_visibility(\'loading\');'.">$size</a></td>
-			<td sorttable_customkey='$timekey'><a id='corePostDL$tableCount' href='#' name='corePostDL$tableCount' onclick=".'toggle_visibility(\'loading\');'.">$modtime</a></td>
+			<td><a id='corePostDL$tableCount' href='#'$favicon class='name' onclick=".'toggle_visibility(\'loadingCommandDiv\');'.">$name</a></td>
+			<td><div><input type='checkbox' name='corePostSelect[]' id='$namehref' value='$namehref' onclick=".'toggle_visibility(\'loadingCommandDiv\');'."</div></td>
+            <td><a id='corePostDL$tableCount' name='corePostDL$tableCount' href='#' onclick=".'toggle_visibility(\'loadingCommandDiv\');'.">$extn</a></td>
+			<td sorttable_customkey='$sizekey'><a id='corePostDL$tableCount' href='#' name='corePostDL$tableCount' onclick=".'toggle_visibility(\'loadingCommandDiv\');'.">$size</a></td>
+			<td sorttable_customkey='$timekey'><a id='corePostDL$tableCount' href='#' name='corePostDL$tableCount' onclick=".'toggle_visibility(\'loadingCommandDiv\');'.">$modtime</a></td>
 		
 		</tr>");
+    $tableCount++;
 ?>
-<script type="text/javascript">
-            jQuery(document).ready(function($){
-                $('#corePostDL<?php echo $tableCount; ?>').on("click",function(){
-                    $.ajax({
-                        url: "cloudCore.php",
-                        type: "POST",
-                        data: { 'filesToDownload': "<?php echo $namehref; ?>", 'download': "1" },
-                        success: function(response){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        },
-                        error: function(){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        }
-                    });
-                });
-            });
-            </script>
-<script type="text/javascript">
-            jQuery(document).ready(function($){
-                $('#dearchiveButton<?php echo $tableCount; ?>').on("click",function(){
-                    $.ajax({
-                        url: "cloudCore.php",
-                        type: "POST", 
-                        data: { 'dearchive': "1", 'dearchiveSelected': "<?php echo $namehref; ?>" },
-                        success: function(response){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        },
-                        error: function(){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        }
-                    });
-                });
-            });
-            </script>            
-<script type="text/javascript">
-            jQuery(document).ready(function($){
-                $('#archiveButton<?php echo $tableCount; ?>').on("click",function(){
-                    $.ajax({
-                        url: "cloudCore.php",
-                        type: "POST", 
-                        data: { 'archiveSelected': "<?php echo $namehref; ?>", 'archive': "1" },
-                        success: function(response){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        },
-                        error: function(){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        }
-                    });
-                });
-            });
-            </script>
-<script type="text/javascript">
-            jQuery(document).ready(function($){
-                $('#archiveButton<?php echo $tableCount; ?>').on("click",function(){
-                    $.ajax({
-                        url: "cloudCore.php",
-                        type: "POST", 
-                        data: { 'archiveSelected': "<?php echo $namehref; ?>", 'archive': "1", 'archextension': "rar" },
-                        success: function(response){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        },
-                        error: function(){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        }
-                    });
-                });
-            });         
-         </script>
-<script type="text/javascript">
-            jQuery(document).ready(function($){
-                $('#archiveButton<?php echo $tableCount; ?>').on("click",function(){
-                    $.ajax({
-                        url: "cloudCore.php",
-                        type: "POST", 
-                        data: { 'archiveSelected': "<?php echo $namehref; ?>", 'archive': "1", 'archextension': "tar" },
-                        success: function(response){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        },
-                        error: function(){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        }
-                    });
-                });
-            });
-            </script>
-<script type="text/javascript">
-            jQuery(document).ready(function($){
-                $('#archiveButton<?php echo $tableCount; ?>').on("click",function(){
-                    $.ajax({
-                        url: "cloudCore.php",
-                        type: "POST", 
-                        data: { 'archiveSelected': "<?php echo $namehref; ?>", 'archive': "1", 'archextension': "tar.bz" },
-                        success: function(response){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        },
-                        error: function(){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        }
-                    });
-                });
-            });
-            </script>          
-<script type="text/javascript">
-            jQuery(document).ready(function($){
-                $('#archiveButton<?php echo $tableCount; ?>').on("click",function(){
-                    $.ajax({
-                        url: "cloudCore.php",
-                        type: "POST", 
-                        data: { 'archiveSelected': "<?php echo $namehref; ?>", 'archive': "1", 'archextension': "tar.bz2" },
-                        success: function(response){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        },
-                        error: function(){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        }
-                    });
-                });
-            });
-            </script>
-<script type="text/javascript">
-            jQuery(document).ready(function($){
-                $('#archiveButton<?php echo $tableCount; ?>').on("click",function(){
-                    $.ajax({
-                        url: "cloudCore.php",
-                        type: "POST", 
-                        data: { 'archiveSelected': "<?php echo $namehref; ?>", 'archive': "1", 'archextension': "tar.gz" },
-                        success: function(response){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        },
-                        error: function(){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        }
-                    });
-                });
-            });  
-            </script>         
-<script type="text/javascript">
-            jQuery(document).ready(function($){
-                $('#archiveButton<?php echo $tableCount; ?>').on("click",function(){
-                    $.ajax({
-                        url: "cloudCore.php",
-                        type: "POST", 
-                        data: { 'archiveSelected': "<?php echo $namehref; ?>", 'archive': "1", 'archextension': "iso" },
-                        success: function(response){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        },
-                        error: function(){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        }
-                    });
-                });
-            });  
-            </script>
-<script type="text/javascript">
-            jQuery(document).ready(function($){
-                $('#archiveButton<?php echo $tableCount; ?>').on("click",function(){
-                    $.ajax({
-                        url: "cloudCore.php",
-                        type: "POST", 
-                        data: { 'archiveSelected': "<?php echo $namehref; ?>", 'archive': "1", 'archextension': "vhd" },
-                        success: function(response){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        },
-                        error: function(){
-                          window.location.replace("http://<?php echo $URL.'/HRProprietary/HRCloud2/'.$FileURL; ?>");
-                        }
-                    });
-                });
-            });  
-            </script>           
 
     <?php
-    $tableCount++;
+
 	   }
 	}
+
+
 	?>
 	</table>
 <div align='center' id='loading' name='loading' style="display:none;"><img src='Resources/pacman.gif'/></div>
 
 </div>
+<script type="text/javascript">
+$(document).ready(function () {
+$("#dearchive").click(function(){
+var dearchiveSelected = new Array();
+$('input[name="corePostSelect[]"]:checked').each(function() {
+dearchiveSelected.push(this.value);
+});
+$.ajax( {
+    type: 'POST',
+    url: 'cloudCore.php',
+    data: { dearchive : "1", filesToDearchive : $('input:checkbox:checked').val()},
+
+    success: function(data) {
+        window.location.href = "cloudCore.php";
+    }
+} );
+
+});
+});
+</script>
 </body>
 </html>
