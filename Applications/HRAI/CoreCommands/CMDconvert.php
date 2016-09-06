fe44413d548cab464ddcf437d706940ea4e27e4a
zelon88 <zelon88@gmail.com>
2016-09-05 21:14:33 -0400

    Updated HRAI

--- Applications/HRAI/CoreCommands/CMDconvert.php
+++ Applications/HRAI/CoreCommands/CMDconvert.php
@@ -6,7 +6,7 @@ echo nl2br('Establishing connection to HRCloud servers.'."\r");
 <script type="text/javascript">
 document.getElementById("HRConvert_Iframe").submit();
 </script></div>
-<iframe id="HRConvert_Iframe" src="http://localhost/HRProprietary/converter/uploadbuttonhtmlNOGUI.php" name="HRConvert" width="275" scrolling="yes" margin-top:-4px; margin-left:-4px; border:double;></iframe>
+<iframe id="HRConvert_Iframe" src="http://localhost/HRProprietary/HRCloud2/Applications/HRConvert2/uploaderNOGUI.php" name="HRConvert" width="275" scrolling="yes" margin-top:-4px; margin-left:-4px; border:double;></iframe>
 </form>
 </div>
 
