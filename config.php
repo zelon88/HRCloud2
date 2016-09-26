6c5f21b8da0df7d138aff391c8c67be09acdbee2
zelon88 <zelon88@gmail.com>
2016-09-26 00:06:58 -0400

    Updated Core. Working on Settings.
    Added basic Settings page.
    -Settings page saves and manages user specific settings.
    Updated Core
    -Tweaked conversions and PDF creation execution scripts.
    Sanitized lots of user input fields.
    -Sanitized most of cloudCore from strange filenames and user queries.
    Changed how logfiles and folders are created/installed.
    -User logs, cachefiles, and config files are now stores in a hidden folder in the users CloudTempDir and is excluded from auto-cleaning operations.
    Made progress on Playlist functionality
    -Started work on HRStreamer to stream, manipulate, and manage user selected media files.
    Random bug fixes and improvements!
    -Woo!!!

--- config.php
+++ config.php
@@ -7,6 +7,22 @@
 // BE SURE TO FILL OUT ALL INFORMATION ACCURATELY !!!
 // PRESERVE ALL SYNTAX AND FORMATTING !!!
 // SERIOUS FILESYSTEM DAMAGE COULD RESULT FROM INCORRECT DATABASE OR DIRECTORY INFO !!!
+// / ------------------------------
+
+
+// / ------------------------------
+// / License Information ...
+// / To continue, please accept the included GPLv3 license by changing the following 
+// / variable to '1'. By changing the '$Accept_GPLv3_OpenSource_License' variable to '1'
+// / you aknowledge that you have read and agree to the terms of the included LICENSE file.
+$Accept_GPLv3_OpenSource_License = '1';
+// / ------------------------------
+
+// / ------------------------------
+// / General Information ... 
+// / Leave '1' for default and to allow individual users to modify their own color-schemes.
+$ColorScheme = '1';
+// / ------------------------------
 
 // / ------------------------------
 // / Admin Login Information ...
@@ -17,29 +33,13 @@
 
 // / ------------------------------  
 // / Security Information ... 
-  
   // / HRCloud2 Server can run on a local machine or on a network as a server to
   // / serve clients over http using standard web browsers. When running locally it is 
-  // / advised to install HRCloud2 in a location that IS NOT hosted. If you do not
-  // / plan on running an online, externally controllable HRCloud2 Server set the online
-  // / setting below to '0' and ignore the rest of the 'Security' settings. HRCloud2 will
-  // / automatically deny access or authorization to external or internal IP's other than the
-  // / localhost machine. 
-
-  // / If you are an administrator or home power-user you may want to run HRCloud2 in a 
-  // / server environment. If you want to make the HRCloud2 admin panel accesible, or if 
-  // / you want to execute HRCloud2 scripts from  from outside your home network change
-  // / the 'Online' setting to '1' and continue to specify the remaining  'Security Settings'.
-
-  // / If 'Online' setting is '1' DocumnentControl Server will allow network 
-  // / users to perform certain tasks and also allow remote admin access from local 
-  // / or remote networks (depending on individual network architecture). 
-  // / If '1', HRCloud2 folder MUST be on a hosted /var/www/html folder or 
-  // / equivilent network accesible location.
-  $Online = '1';  
+  // / advised to install HRCloud2 in a location that IS NOT hosted. 
+  // / Unique Salts.
+  $Salts = 'somethingSoRanDoMThatNobod65y_Will_evar+guess+it';
   // / Internal IP Address.
   $InternalIP = '192.168.1.7';
-
   // / Externally or internally accesible domain or IP.
   $URL = 'localhost';
   // / Scan for viruses during directory scan. Use 1 for true or leave blank for false. 
@@ -55,13 +55,4 @@
   $InstLoc = '/var/www/html/HRProprietary/HRCloud2';
   // / Directory to be scanned for file dumps (NO SLASH AFTER DIRECTORY!!!) ...  
   $CloudLoc = '/home/justin/Desktop/TestDir/Cloud';
-  // / If you want HRCloud2 to create and manage a synced copy of your 
-   // / Output Location, set AutoBackups below to '1', 
-  $AutoBackup = '1';
-  // / If you have enabled Auto Backups (set AutoBackups to '1'), select a root directory
-   // / for organized backup files (NO SLASH AFTER DIRECTORY!!! Leave blank for none.) ...
-  $BackupLoc = '/home/justin/Desktop/TestDir/BACKUP';
-  // / If you have enabled Auto Backups (set AutoBackups to '1'), select a root directory
-   // / for organized backup files (NO SLASH AFTER DIRECTORY!!! Leave blank for none.) ...
-  $ActionLoc = '/home/justin/Desktop/TestDir/ActionItems';
 // / ------------------------------ 
