f0e1315cb0e804d4daeea99f44aa2abc5f91d73c
zelon88 <zelon88@gmail.com>
2016-11-28 01:02:51 -0500

    Added Calculator App!
    -Calculator works with simple math equations and plain speech.
    -Gives a simple breakdown of the logic used to perform the equation.
    -Super-Duper floating-point accuracy (no JScript!)

---
+++ Applications/Calculator/Calculator.php
@@ -0,0 +1,84 @@
+<?php
+
+/*//
+HRCLOUD2-PLUGIN-START
+App Name: Calculator
+App Version: 1.0 (11-27-2016 00:00)
+App License: GPLv3
+App Author: zelon88
+App Description: A simple HRCloud2 App for doing math.
+App Integration: 0 (False)
+HRCLOUD2-PLUGIN-END
+//*/
+
+?>
+<script src="sorttable.js"></script>
+<script type="text/javascript">
+// / Javascript to clear the newNote text input field onclick.
+    function Clear() {    
+      document.getElementById("calculatorInput").value= ""; }
+</script>
+<div id='CalculatorAPP' name='CalculatorAPP' align='center'><h3>Calculator</h3><hr />
+
+<form action="Calculator.php" method="post">
+<input id="calculatorInput" name="calculatorInput" value="" type="text">
+<input type="submit" id="calculatorSubmit" name="calculatorSubmit" title="Perform Equation" alt="Perform Equation" value="Perform Equation"></form>
+<hr />
+<?php
+
+// / The follwoing code checks if the commonCore.php file exists and 
+// / terminates if it does not.
+if (!file_exists('/var/www/html/HRProprietary/HRCloud2/commonCore.php')) {
+  echo nl2br('</head><body>ERROR!!! HRC2CalculatorApp26, Cannot process the HRCloud2 Common Core file (commonCore.php)!'."\n".'</body></html>'); 
+  die (); }
+else {
+  require_once ('/var/www/html/HRProprietary/HRCloud2/commonCore.php'); }
+
+if (isset($_POST['calculatorInput'])) {
+  // / The following code sets the global variables for the session.
+  $numbers = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+  $basicFunctions = str_split('+-xX*/');
+  $advancedFunctions = str_split('%');
+  $additionFunctions = str_split('+'); 
+  $subtractionFunctions = str_split('-');
+  $multicationFunctions = str_split('*');
+  $divisionFunctions = str_split('/');
+  // / The following code sets the POST data to variables for the session.
+  $calculatorInput = str_replace(str_split('[]{};:$!#&@><'), '', $_POST['calculatorInput']);
+  $calculatorInput = strtolower($calculatorInput);
+  $calculatorInput = str_replace(str_split('xX'), '*', $calculatorInput);
+  $calculatorInput = str_replace('multiplied by', '*', $calculatorInput);
+  $calculatorInput = str_replace('plus', '+', $calculatorInput);
+  $calculatorInput = str_replace('and', '+', $calculatorInput);
+  $calculatorInput = str_replace('minus', '-', $calculatorInput);
+  $calculatorInput = str_replace('take away', '-', $calculatorInput);
+  $calculatorInput = str_replace('times', '*', $calculatorInput);
+  $calculatorInput = str_replace('divided by', '/', $calculatorInput);
+  $calculatorInput = str_replace(' ', '', $calculatorInput);
+  $calculatorArguments = str_replace(str_split('[]{};:$!#&@><'), '', $calculatorInput);
+  // / The following code cleans the user input for the math interpreter.
+  $counter = 0; 
+  foreach ($numbers as $number) {
+    $calculatorInput = str_replace($number.'(', $number.'*(', $calculatorInput);
+    $calculatorInput = str_replace(')'.$number, ')*'.$number, $calculatorInput); }
+  if ($calculatorInput == '') {
+    echo ('There was no equation to calculate!'); }
+
+  // / The following code defines basic and advanced functions for the math interpreter.
+
+  $priorityFunctions = get_string_between($calculatorInput, '(', ')');
+  $priorityFunctions = str_replace(str_split('()'), '', $priorityFunctions);
+  if ($priorityFunctions !== '') {
+    $counter++;
+    echo ('<i>'.$calculatorInput.'</i>');
+    eval('$priorityTotal = ('.$priorityFunctions.');');
+    echo ('<p><strong>'.$counter.'.</strong> <i>('.$priorityFunctions.')</i> = <strong>'.$priorityTotal.'</strong></p>');
+    $calculatorInput = str_replace($priorityFunctions, $priorityTotal, $calculatorInput); }
+
+if ($calculatorInput !== '') {
+  $counter++;
+  eval('$total = ('.$calculatorInput.');');
+  echo ('<p><strong>'.$counter.'.</strong> <i>'.$calculatorInput.'</i> = <u><strong>'.$total.'</strong></u></p>'); } }
+
+?>
+</div>
