f6eb244ad015d324e9c8ca0d33af97cc557e0894
zelon88 <zelon88@gmail.com>
2016-12-04 01:11:28 -0500

    Fixed bugs.

--- Applications/displaydirectorycontents_logs1/.index.php
+++ Applications/displaydirectorycontents_logs1/.index.php
@@ -1,138 +1,91 @@
 <!doctype html>
 <html>
 <head>
-   <meta charset="UTF-8">
-   <link rel="shortcut icon" href="./.favicon.ico">
-   <title>HRCLoud2 | Log Viewer</title>
-
-   <link rel="stylesheet" href="./.style.css">
-   <script src="./.sorttable.js"></script>
+  <meta charset="UTF-8">
+  <link rel="shortcut icon" href="./.favicon.ico">
+  <title>HRCLoud2 | Log Viewer</title>
+  <link rel="stylesheet" href="./.style.css">
+  <script src="./.sorttable.js"></script>
 </head>
 <body>
 <div id="container">
-	<div align="center">
-	<h3>HRCloud2 Logs</h3>
-    </div>
-	<table class="sortable">
-	    <thead>
-		<tr>
-			<th>Filename</th>
-			<th>Type</th>
-			<th>Size</th>
-			<th>Date Modified</th>
-		</tr>
-	    </thead>
-	    <tbody><?php
+  <div align="center"><h3>HRCloud2 Logs</h3></div>
+<table class="sortable">
+<thead>
+  <tr>
+    <th>Filename</th>
+    <th>Type</th>
+    <th>Size</th>
+    <th>Date Modified</th>
+  </tr>
+  </thead>
+<tbody>
+<?php
+function pretty_filesize($file) {
+  $size=filesize($file);
+  if($size<1024){$size=$size." Bytes"; }
+  elseif(($size<1048576)&&($size>1023)){$size=round($size/1024, 1)." KB";}
+  elseif(($size<1073741824)&&($size>1048575)){$size=round($size/1048576, 1)." MB";}
+  else{$size=round($size/1073741824, 1)." GB"; }
+  return $size; }
 
-	// Adds pretty filesizes
-	function pretty_filesize($file) {
-		$size=filesize($file);
-		if($size<1024){$size=$size." Bytes";}
-		elseif(($size<1048576)&&($size>1023)){$size=round($size/1024, 1)." KB";}
-		elseif(($size<1073741824)&&($size>1048575)){$size=round($size/1048576, 1)." MB";}
-		else{$size=round($size/1073741824, 1)." GB";}
-		return $size;
-	}
 
- 	// Checks to see if veiwing hidden files is enabled
-	if($_SERVER['QUERY_STRING']=="hidden")
-	{$hide="";
-	 $ahref="./";
-	 $atext="Hide";}
-	else
-	{$hide=".";
-	 $ahref="./?hidden";
-	 $atext="Show";}
+if($_SERVER['QUERY_STRING']=="hidden") {
+  $hide="";
+  $ahref="./";
+  $atext="Hide"; }
+else {
+  $hide=".";
+  $ahref="./?hidden";
+  $atext="Show"; }
+$myDirectory=opendir(".");
+while($entryName=readdir($myDirectory)) {
+  $dirArray[]=$entryName; }
+  closedir($myDirectory);
+  $indexCount=count($dirArray);
+  sort($dirArray);
 
-	 // Opens directory
-	 $myDirectory=opendir(".");
+for($index=0; $index < $indexCount; $index++) {
+  if(substr("$dirArray[$index]", 0, 1)!=$hide) {
+    $favicon="";
+    $class="file";
+    $name=$dirArray[$index];
+    $namehref=$dirArray[$index];
+    $modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
+    $timekey=date("YmdHis", filemtime($dirArray[$index]));
 
-	// Gets each entry
-	while($entryName=readdir($myDirectory)) {
-	   $dirArray[]=$entryName;
-	}
+	if(is_dir($dirArray[$index])) {
+	  $extn="&lt;Directory&gt;";
+	  $size="&lt;Directory&gt;";
+	  $sizekey="0";
+	  $class="dir";
 
-	// Closes directory
-	closedir($myDirectory);
-
-	// Counts elements in array
-	$indexCount=count($dirArray);
-
-	// Sorts files
-	sort($dirArray);
-
-	// Loops through the array of files
-	for($index=0; $index < $indexCount; $index++) {
-
-	// Decides if hidden files should be displayed, based on query above.
-	    if(substr("$dirArray[$index]", 0, 1)!=$hide) {
-
-	// Resets Variables
-		$favicon="";
-		$class="file";
-
-	// Gets File Names
-		$name=$dirArray[$index];
-		$namehref=$dirArray[$index];
-
-	// Gets Date Modified
-		$modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
-		$timekey=date("YmdHis", filemtime($dirArray[$index]));
-
-	// Separates directories, and performs operations on those directories
-		if(is_dir($dirArray[$index]))
-		{
-				$extn="&lt;Directory&gt;";
-				$size="&lt;Directory&gt;";
-				$sizekey="0";
-				$class="dir";
-
-			// Gets favicon.ico, and displays it, only if it exists.
-				if(file_exists("$namehref/favicon.ico"))
-					{
-						$favicon=" style='background-image:url($namehref/favicon.ico);'";
-						$extn="&lt;Website&gt;";
-					}
-
-			// Cleans up . and .. directories
-				if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;"; $favicon=" style='background-image:url($namehref/.favicon.ico);'";}
-				if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
-		}
-
-	// File-only operations
-		else{
-			// Gets file extension
-			$extn=pathinfo($dirArray[$index], PATHINFO_EXTENSION);
-
-			// Prettifies file type
-			switch ($extn){
-
-				case "txt": $extn="Text File"; break;
-
-				default: if($extn!=""){$extn=strtoupper($extn)." File";} else{$extn="Unknown";} break;
-			}
-
-			// Gets and cleans up file size
-				$size=pretty_filesize($dirArray[$index]);
-				$sizekey=filesize($dirArray[$index]);
-		}
-		if ($namehref == 'index.html' or $namehref == 'style.css' or $namehref == 'Notes' or $namehref == 'Contacts' 
-		  or strpos($namehref, '.css') == 'true' or strpos($namehref, '.html') == 'true' or strpos($namehref, '.php') == 'true') continue;
-	// Output
-	 echo("
-		<tr class='$class'>
+	  if(file_exists("$namehref/favicon.ico")) {
+	    $favicon=" style='background-image:url($namehref/favicon.ico);'";
+	    $extn="&lt;Website&gt;"; }
+	  if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;"; $favicon=" style='background-image:url($namehref/.favicon.ico);'";}
+	  if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;"; } }
+	  
+	  else {
+		$extn=pathinfo($dirArray[$index], PATHINFO_EXTENSION);
+		switch ($extn) {
+		  case "txt": $extn="Text File"; break;
+		  default: if($extn!=""){$extn=strtoupper($extn)." File";} else{$extn="Unknown";} 
+		  break; }
+		$size=pretty_filesize($dirArray[$index]);
+		$sizekey=filesize($dirArray[$index]); }
+	  if ($namehref == 'index.html' or $namehref == 'style.css' or $namehref == 'Notes' or $namehref == 'Contacts' 
+	    or strpos($namehref, '.css') == 'true' or strpos($namehref, '.html') == 'true' or strpos($namehref, '.php') == 'true') continue;
+	   echo("
+		  <tr class='$class'>
 			<td><a href='$namehref'$favicon class='name'>$name</a></td>
 			<td><a href='$namehref'>$extn</a></td>
 			<td sorttable_customkey='$sizekey'><a href='./$namehref'>$size</a></td>
 			<td sorttable_customkey='$timekey'><a href='./$namehref'>$modtime</a></td>
-		</tr>");
-	   }
-	}
-	usleep(500);
-	?>
-
-	    </tbody>
-	</table>
+		  </tr>"); } }
+?>
+</tbody>
+</table>
 
 </div>
 </body>
