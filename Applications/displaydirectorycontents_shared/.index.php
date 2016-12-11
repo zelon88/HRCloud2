679cc10b392fb644ba6008a0425627b7c958254d
zelon88 <zelon88@gmail.com>
2016-12-10 22:51:04 -0500

    Working on Shared directories.
    -Reformatted, re-ordered, and re-wrote some code.

--- Applications/displaydirectorycontents_shared/.index.php
+++ Applications/displaydirectorycontents_shared/.index.php
@@ -6,34 +6,72 @@
    <title>HRCLoud2 | Shared Files Viewer</title>
 
    <link rel="stylesheet" href="./.style.css">
-<script type="text/javascript" src="Applications/jquery-3.1.0.min.js"></script>
+   <script type="text/javascript" src="/HRProprietary/HRCloud2/Applications/jquery-3.1.0.min.js"></script>
    <script src="./.sorttable.js"></script>
 <script type="text/javascript">
 function goBack() {
   window.history.back(); }
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+      e.style.display = 'none';
+    else
+      e.style.display = 'block'; }
 </script>
 </head>
 <body>
 <div id="container">
 <div align="center"><h3>HRCloud2 Shared Files</h3></div>
 <div align="center" style="margin-bottom:10px;"><form><input type='submit' name="refresh" id="refresh" value='&#x21BA' href="#" class="submitsmall" onclick="toggle_visibility('loadingCommandDiv');"></form></div>
-
-<table class="sortable">
-  <thead>
-	<tr>
-	  <th>Filename</th>
-	  <th>Type</th>
-	  <th>Un-Share</th>
-	  <th>Size</th>
-	  <th>Date Modified</th>
-	</tr>
-	  </thead>
-	  <tbody><?php
+<div align="center" name="unshareButton" id="unshareButton" style="margin-bottom:10px;"><img src="/HRProprietary/HRCloud2/Resources/deletesmall.png" title="Remove Shared File" alt="Remove Shared File" onclick="toggle_visibility('loadingCommandDiv');"></div>
+<div align="center" id='loadingCommandDiv' name='loadingCommandDiv' style="display:none; margin-bottom:10px; max-width:64px; max-height:64px;"><img src='/HRProprietary/HRCloud2/Resources/logosmall.gif'></div>
+<?php
+// / Tje following code verifies that the HRCloud2 configuration file exists.
 if (!file_exists('/var/www/html/HRProprietary/HRCloud2/config.php')) {
-  echo nl2br('</head>ERROR!!! HRC2Index19, Cannot process the HRCloud2 configuration file (config.php).'."\n"); 
+  echo nl2br('</head>ERROR!!! HRC2SharedIndex19, Cannot process the HRCloud2 configuration file (config.php).'."\n"); 
   die (); }
 else {
   require('/var/www/html/HRProprietary/HRCloud2/config.php'); }
+
+$PLMediaArr =  array('mp2', 'mp3', 'wma', 'wav', 'aac', 'flac', 'ogg', 'avi', 'mov', 'mkv', 'flv', 'ogv', 'wmv', 'mpg', 'mpeg', 'm4v', '3gp', 'mp4');
+$archArr = array('rar', 'tar', 'tar.bz', '7z', 'zip', 'tar.gz', 'tar.bz2', 'tgz');
+$pdfWordArr = array('pdf', 'doc', 'docx', 'txt', 'rtf', 'odf', 'pages', 'jpg', 'jpeg', 'png', 'bmp', 'gif');
+$convertArr = array('pdf', 'doc', 'docx', 'txt', 'rtf', 'odf', 'pages', 'jpg', 'jpeg', 'png', 'bmp', 'gif', 'mp2', 'mp3', 'wma', 'wav', 'aac', 'flac', 'ogg', 'avi', 'mov', 'mkv', 'flv', 'ogv', 'wmv', 'mpg', 'mpeg', 'm4v', '3gp', 'mp4');
+$pdfWordArr = array('pdf', 'jpg', 'jpeg', 'png', 'bmp', 'gif');
+$imgArr = array('jpg', 'jpeg', 'png', 'bmp', 'gif');
+// / Verify that WordPress is installed.
+$WPFile = '/var/www/html/wp-load.php';
+if (!file_exists($WPFile)) {
+  echo nl2br('</head>ERROR!!! HRC2SharedIndex27, WordPress was not detected on the server.'."\n"); }
+  else {
+    require_once($WPFile); } 
+$UserIDRAW = get_current_user_id();
+$UserID = hash('ripemd160', $UserIDRAW.$Salts);
+$UserContacts = $InstLoc.'/DATA/'.$UserID.'/.AppData/.contacts.php';
+$UserSharedIndex = $URL.'/HRProprietary/HRCloud2/DATA/'.$UserID.'/.AppData/Shared/.index.php';
+$UserNotes = $InstLoc.'/DATA/'.$UserID.'/.AppData/.notes.php';
+$UserConfig = $InstLoc.'/DATA/'.$UserID.'/.AppData/.config.php';
+if (!file_exists($UserConfig)) {
+  @chmod($UserConfig, 0755); }
+if (!file_exists($UserConfig)) {
+  echo nl2br('</head>ERROR!!! HRC2SharedIndex35, User Cache file was not detected on the server!'."\n"); 
+  die (); }
+else {
+    require($UserConfig); } 
+
+if ($ColorScheme == '0' or $ColorScheme == '' or !isset($ColorScheme)) {
+  $ColorScheme = '1'; }
+if ($ColorScheme == '1') {
+  echo ('<link rel="stylesheet" type="text/css" href="/HRProprietary/HRCloud2/Applications/displaydirectorycontents_72716/style.css">'); }
+if ($ColorScheme == '2') {
+  echo ('<link rel="stylesheet" type="text/css" href="/HRProprietary/HRCloud2/Applications/displaydirectorycontents_72716/styleRED.css">'); }
+if ($ColorScheme == '3') {
+  echo ('<link rel="stylesheet" type="text/css" href="/HRProprietary/HRCloud2/Applications/displaydirectorycontents_72716/styleGREEN.css">'); }
+if ($ColorScheme == '4') {
+  echo ('<link rel="stylesheet" type="text/css" href="/HRProprietary/HRCloud2/Applications/displaydirectorycontents_72716/styleGREY.css">'); }
+if ($ColorScheme == '5') {
+  echo ('<link rel="stylesheet" type="text/css" href="/HRProprietary/HRCloud2/Applications/displaydirectorycontents_72716/styleBLACK.css">'); } 
+
 $fileCounter = 0;
 
 	function pretty_filesize($file) {
@@ -89,18 +127,36 @@ $fileCounter = 0;
 
 		if ($namehref == 'index.html' or $namehref == 'style.css' or $namehref == 'Notes' or $namehref == 'Contacts' 
 			or strpos($namehref, '.css') == 'true' or strpos($namehref, '.html') == 'true' or strpos($namehref, '.css') == 'true' 
-			or strpos($namehref, 'Shared') == 'true') continue;
+			or strpos($namehref, 'Shared') == 'true') continue; ?>
+<table class="sortable">
+  <thead>
+	<tr>
+	  <th>Filename</th>
+	  <th>Type</th>
+	  <th>Select</th>
+	  <th>Size</th>
+	  <th>Date Modified</th>
+	</tr>
+	  </thead>
+	  <tbody>
+<?php
 	 echo("<tr class='$class'>
 			<td><a href='./$namehref'$favicon class='name'>$name</a></td>
 			<td><a href='./$namehref'>$extn</a></td>
-			<td><div id='$namehref' name='UnShare$fileCounter' onclick=\"UnShare$fileCounter(); toggle_visibility('loadingCommandDiv');\" ><img src='$URL/HRProprietary/HRCloud2/Resources/x.png'></div></td>
-			<td sorttable_customkey='$sizekey'><a href='./$namehref'>$size</a></td>
+      <td><div><input type='checkbox' name='corePostSelect[]' id='$namehref' value='$namehref'></div></td>
+            <td sorttable_customkey='$sizekey'><a href='./$namehref'>$size</a></td>
 			<td sorttable_customkey='$timekey'><a href='./$namehref'>$modtime</a></td>
-		</tr>"); ?>
+		</div></tr>"); ?>
+<?php 
+$fileCounter++;
+} } ?>
 <script type="text/javascript">
-function UnShare<?php echo $fileCounter; ?>() {
-$("#UnShare<?php echo $fileCounter; ?>").click(function(){
-var unshareSelected = $('#UnShare<?php echo $fileCounter; ?>').attr('id');
+$(document).ready(function () {
+$("#unshareButton").click(function(){
+var unshareSelected = new Array();
+$('input[name="corePostSelect[]"]:checked').each(function() {
+unshareSelected.push(this.value);
+});
 $.ajax( {
     type: 'POST',
     url: 'cloudCore.php',
@@ -110,14 +166,9 @@ $.ajax( {
     }
 } );
 });
-};
+});
 </script>
-<?php 
-$fileCounter++;
-} } ?>
-	    </tbody>
-	</table>
-
-</div>
+</tbody>
+</table>
 </body>
 </html>
