ac691f0cd3ccbd3c055dadfb5ecc6a82b38d9a4e
zelon88 <zelon88@gmail.com>
2016-12-25 00:37:20 -0500

    Grabber works!!!
    -Users can now aim Grabber at url's containing files to have them saved to their Cloud drive.

--- Applications/Grabber/Grabber.php
+++ Applications/Grabber/Grabber.php
@@ -28,7 +28,7 @@ $DangerArr = array('<script ', '<?', '?>');
       document.getElementById("grabberFilename").value= ""; }
 </script>
 <div id='GrabberAPP' name='GrabberAPP' align='center'><h3>File Grabber</h3><hr />
-<form action="Grabber.php" method="post">
+<form action="Grabber.php" method="post" enctype="multipart/form-data">
 <?php 
 if (!isset($grabberURLPOST)) { 
   echo ('<p align="left" style="padding-left:15px;"><strong>2. </strong>Enter a URL to download.</p>'); 
@@ -37,10 +37,9 @@ if (!isset($grabberURLPOST)) {
 <?php 
   echo ('<p align="left" style="padding-left:15px;"><strong>1. </strong>Enter a Cloud directory/filename for your downloaded file (w/ extension).</p>'); 
   echo('<p align="left" style="padding-left:15px;"><input  id="grabberFilename" name="grabberFilename" value="" type="text"></p>'); 
-  echo ('<p align="left" style="padding-left:15px;"><input type="submit" id="grabberSubmit" name="grabberSubmit" title="Grab Files" alt="Grab Files" value="Grab Files"></p>'); } 
+  echo ('<p align="left" style="padding-left:15px;"><input type="submit" id="grabberSubmit" name="grabberSubmit" title="Grab Files" alt="Grab Files" value="Grab Files"></p><hr />'); } 
 ?>
 </form>
-<hr />
 <?php
 // / The follwoing code checks if the commonCore.php file exists and 
 // / terminates if it does not.
@@ -50,12 +49,12 @@ if (!file_exists('/var/www/html/HRProprietary/HRCloud2/commonCore.php')) {
 else {
   require_once ('/var/www/html/HRProprietary/HRCloud2/commonCore.php'); }
 
-$GrabberFile = $CloudUsrDir.$GrabberFilenamePOST;
+  $GrabberFile = $CloudUsrDir.$GrabberFilenamePOST;  
 
 // / The following function was found on 
  // / http://stackoverflow.com/questions/7684771/how-check-if-file-exists-from-the-url
 // / It checks if a remote file exists and returns the status. 
-function does_url_exist($url){
+function does_url_exist($url) {
   $ch = curl_init($url);    
   curl_setopt($ch, CURLOPT_NOBODY, true);
   curl_exec($ch);
@@ -69,7 +68,7 @@ function does_url_exist($url){
 
 function grab_simple_file($url, $filename) {
   set_time_limit(0);
-  $fp = fopen (dirname(__FILE__) . $GrabberFile, 'w+');
+  $fp = fopen ($filename, 'w+');
   $ch = curl_init(str_replace(" ","%20",$url));
   curl_setopt($ch, CURLOPT_TIMEOUT, 50);
   curl_setopt($ch, CURLOPT_FILE, $fp); 
@@ -78,31 +77,20 @@ function grab_simple_file($url, $filename) {
   curl_close($ch);
   fclose($fp); }
 
-if (isset($_POST['grabberURL'])) {
-  if (strpos($grabberURLPOST, 'http') == 'false') {
+if (isset($grabberURLPOST)) {
+  $GrabberFile = $CloudUsrDir.$GrabberFilenamePOST;  
+  if (strpos('http', $grabberURLPOST) == 'false') {
     $txt = ('ERROR!!! HRC2GrabberApp43, The supplied URL "'.$grabberURLPOST.'" is not valid on '.$Time.'!'); 
     echo ($txt.'<hr />');
     $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL , FILE_APPEND); }
   if (strpos($grabberURLPOST, 'http') == 'true') {
-    // / The following code is performed on videos or files obfuscated by HTML..
-    foreach ($YouTubeArr as $YouTubeArr1) {
-      if (strpos($grabberURLPOST, $YouTubeArr1) == 'true') {
-        $txt = ('ERROR!!! HRC2GrabberApp96, Grabber currently does not support embedded files on '.$Time.'!');
-        echo ($txt.'<hr />');
-        $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL , FILE_APPEND); 
-        die();
-        $Continue = '1'; }
-
-      if ($Contiue = '1') {
-        if (does_url_exist($grabberURLPOST) == 'true') { } }
-
+    if (does_url_exist($grabberURLPOST) == 'true') { 
     // / The following code is performed on normal URL's and files.
-    if (strpos($grabberURLPOST, $YouTubeArr1) == 'false') {
       $txt = ('OP-Act: Scanning URL with simple method for file on '.$Time.'.');
       echo ($txt.'<hr />');
       $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL , FILE_APPEND);
       if (does_url_exist($grabberURLPOST) == 'true') {
-        $GRABData = grab_simple_file($grabberURLPOST, $GrabberFilenamePOST); 
+        $GRABData = grab_simple_file($grabberURLPOST, $GrabberFile); 
         if (!file_exists($GrabberFile)) {
           $txt = ('ERROR!!! HRC2GrabberApp115, There was a problem creating '.$GrabberFilenamePOST.' on '.$Time.'!'); 
           echo ($txt.'<hr />');
@@ -110,14 +98,14 @@ if (isset($_POST['grabberURL'])) {
         if (file_exists($GrabberFile)) {
           $txt = ('OP-Act: Created '.$GrabberFilenamePOST.' on '.$Time.'!'); 
           echo ($txt.'<hr />');
-          $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL , FILE_APPEND); } } }
+          $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL , FILE_APPEND); } }
 
-      }
+
 
       if (does_url_exist($grabberURLPOST) == 'false') {
         $txt = ('ERROR!!! HRC2GrabberApp70, The supplied URL contains reference to a file that does not exist on '.$Time.'!');
         echo ($txt.'<hr />');
-        $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL , FILE_APPEND); } } }
+        $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL , FILE_APPEND); } } } }
 
 
 ?>
