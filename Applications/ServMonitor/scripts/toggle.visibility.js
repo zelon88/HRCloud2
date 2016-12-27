5c96141005be7f2dd26bd9998886a39b378ec8f3
zelon88 <zelon88@gmail.com>
2016-12-27 01:02:26 -0500

    Working on ServMonitor App!
    This is gonna be wicked cool!
    -Graphs and real-time monitoring for your server.
    -Cool responsive gauges.
    -More information than you can shake a stick at. (in-progress)
    -Currently does CPU (average past 5 minutes, no real-time) and RAM (& used and quantity used / free in GB, no real-time).

---
+++ Applications/ServMonitor/scripts/toggle.visibility.js
@@ -0,0 +1,24 @@
+    function toggle_visibility(id) {
+      var e = document.getElementById(id);
+      if(e.style.display == 'none')
+         e.style.display = 'block';
+      else
+         e.style.display = 'none'; }
+    function toggle_visibility1(id) {
+      var e = document.getElementById(id);
+      if(e.style.display == 'none')
+         e.style.display = 'block';
+      else
+         e.style.display = 'none'; }
+    function toggle_visibilitySHOW(id) {
+      var e = document.getElementById(id);
+      if(e.style.display == 'none')
+         e.style.display = 'block';
+      else
+         e.style.display = 'block'; }
+    function toggle_visibilityHIDE(id) {
+      var e = document.getElementById(id);
+      if(e.style.display == 'block')
+         e.style.display = 'none';
+      else
+         e.style.display = 'none'; }
