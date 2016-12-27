d348d39dbe51e1f748e3d0ab792ff7cb9b611ea6
zelon88 <zelon88@gmail.com>
2016-12-27 00:46:42 -0500

    Delete cpuUpdater.php

--- cpuUpdater.php
+++
@@ -1,6 +0,0 @@
-<?php
-// / The following code will return the server's CPU load percentage average for the past 5 minutes.
-$exec_loads = sys_getloadavg();
-$exec_cores = trim(shell_exec("grep -P '^processor' /proc/cpuinfo|wc -l"));
-$cpu = round($exec_loads[1]/($exec_cores + 1)*100, 0);
-?>
