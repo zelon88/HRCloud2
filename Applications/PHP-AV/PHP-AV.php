51553162cb2adbd460cbce7985ba8a9e8e165afd
zelon88 <zelon88@gmail.com>
2017-01-12 23:32:22 -0500

    PHP-AV App, Updated Virus Definitions.

--- Applications/PHP-AV/PHP-AV.php
+++ Applications/PHP-AV/PHP-AV.php
@@ -11,7 +11,7 @@ App Integration: 0 (False)
 App Permission: 0 (Admin)
 HRCLOUD2-PLUGIN-END
 //*/
-$versions = 'PHP-AV App v2.0 | Virus Definition v1.2';
+$versions = 'PHP-AV App v2.0 | Virus Definition v1.3, 1/12/2017';
 ?>
 <script type="text/javascript">
     function Clear() {    
