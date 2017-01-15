2c1c34145fa6464f63fcbe412d945c696f0cfca2
zelon88 <zelon88@gmail.com>
2017-01-15 00:09:37 -0500

    v0.9.9.8.3... A stable release.
    -v0.9.9.8.3
    -Fixed bugs.
    -Fixed syntax errors.
    -Updated PHP-AV APP to include SHA256 hashing of files.
    -Updated PHP-AV App virus definitions to include SHA256 data.
    -Updated PHP-AV App virus definitions.

--- compatibilityCore.php
+++ compatibilityCore.php
@@ -2,8 +2,8 @@
 
 /*
 HRCLOUD2 VERSION INFORMATION
-THIS VERSION : v0.9,9.8.2
-WRITTEN ON : 1/13/2017
+THIS VERSION : v0.9,9.8.3
+WRITTEN ON : 1/15/2017
 */
 
 echo ('<div style="margin-left:15px;">');
