2c4a6a7cf3287d24e3df2c4e0225dfc34a5e7702
zelon88 <zelon88@gmail.com>
2017-01-26 23:16:48 -0500

    v0.9.9.9.1, Updated ServMon, PHP-AV, & Executor.
    -ServMon: v1.5, Working on backend networking detection. Drive status works. Minor UI improvements.
    -PHP-AV: v2.2, New definitions. Minor bug-fixes.
    -Executor: v1.2, Fixed syntax errors. Dropped support for PHP code interpretation. Improved error reporting. This version actually works!

--- compatibilityCore.php
+++ compatibilityCore.php
@@ -2,8 +2,8 @@
 
 /*
 HRCLOUD2 VERSION INFORMATION
-THIS VERSION : v0.9,9.9
-WRITTEN ON : 1/25/2017
+THIS VERSION : v0.9,9.9.1
+WRITTEN ON : 1/26/2017
 */
 
 echo ('<div style="margin-left:15px;">');
