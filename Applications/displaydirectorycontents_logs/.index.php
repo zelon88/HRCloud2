66ee62e43a6528f57aef5b22eeecfdcb8ea0679c
zelon88 <zelon88@gmail.com>
2017-03-03 23:11:01 -0500

    v1,3 - Improved security & quality. Fixed bugs.
    -Improved security by adding authentication to log directories.
    -Logs will only display to the user that generated them, even if the URL is guessed.
    -Improved code quality + consistency.
    -Improved comments.

--- Applications/displaydirectorycontents_logs/.index.php
+++ Applications/displaydirectorycontents_logs/.index.php
@@ -12,32 +12,48 @@
 <div id="container">
 <div align="center"><h3>HRCloud2 Logs</h3></div>
 <?php
-// / Tje following code verifies that the HRCloud2 configuration file exists.
+// / -----------------------------------------------------------------------------------
+// / The following code verifies that the HRCloud2 configuration file exists.
 if (!file_exists('/var/www/html/HRProprietary/HRCloud2/config.php')) {
-  echo nl2br('</head>ERROR!!! HRC2SharedIndex19, Cannot process the HRCloud2 configuration file (config.php).'."\n"); 
+  echo nl2br('</head>ERROR!!! HRC2LogsIndex19, Cannot process the HRCloud2 configuration file (config.php).'."\n"); 
   die (); }
 else {
   require('/var/www/html/HRProprietary/HRCloud2/config.php'); }
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / The following code verifies that WordPress is installed.
 $WPFile = '/var/www/html/wp-load.php';
-// / Verify that WordPress is installed.
 if (!file_exists($WPFile)) {
-  echo nl2br('</head>ERROR!!! HRC2SharedIndex27, WordPress was not detected on the server.'."\n"); }
+  echo nl2br('</head>ERROR!!! HRC2LogsIndex27, WordPress was not detected on the server.'."\n"); }
   else {
     require_once($WPFile); } 
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / The following code sets the variables for the session.
+$Current_URL = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $UserIDRAW = get_current_user_id();
 $UserID = hash('ripemd160', $UserIDRAW.$Salts);
 $UserContacts = $InstLoc.'/DATA/'.$UserID.'/.AppData/.contacts.php';
 $UserSharedIndex = $URL.'/HRProprietary/HRCloud2/DATA/'.$UserID.'/.AppData/Shared/.index.php';
 $UserNotes = $InstLoc.'/DATA/'.$UserID.'/.AppData/.notes.php';
 $UserConfig = $InstLoc.'/DATA/'.$UserID.'/.AppData/.config.php';
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / The following code loads the users config.php file.
 if (!file_exists($UserConfig)) {
   @chmod($UserConfig, 0755); }
 if (!file_exists($UserConfig)) {
-  echo nl2br('</head>ERROR!!! HRC2SharedIndex35, User Cache file was not detected on the server!'."\n"); 
+  echo nl2br('</head>ERROR!!! HRC2LogsIndex49, User Cache file was not detected on the server!'."\n"); 
   die (); }
 else {
     require($UserConfig); } 
+// / -----------------------------------------------------------------------------------
 
+// / -----------------------------------------------------------------------------------
+// / The following code loads the users color scheme.
 if ($ColorScheme == '0' or $ColorScheme == '' or !isset($ColorScheme)) {
   $ColorScheme = '1'; }
 if ($ColorScheme == '1') {
@@ -50,6 +66,18 @@ if ($ColorScheme == '4') {
   echo ('<link rel="stylesheet" type="text/css" href="/HRProprietary/HRCloud2/Applications/displaydirectorycontents_72716/styleGREY.css">'); }
 if ($ColorScheme == '5') {
   echo ('<link rel="stylesheet" type="text/css" href="/HRProprietary/HRCloud2/Applications/displaydirectorycontents_72716/styleBLACK.css">'); } 
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / The following code will verify the user ID and the directory being accessed before continuing.
+if (strpos($Current_URL, $UserID) == 'true' && strpos($Current_URL, '/Shared') == 'false') {
+  $txt = ('ERROR!!! HRC2LogsIndex74, Could not verify the user '.$UserID.' on '.$Time.'!');
+  $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND); 
+  die($txt); }
+// / ------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / The following code generates most of the HTML boody for the session.
 ?>
 <table class="sortable">
   <thead>
@@ -70,7 +98,6 @@ if ($ColorScheme == '5') {
 	  elseif(($size<1073741824)&&($size>1048575)){$size=round($size/1048576, 1)." MB";}
 	  else{$size=round($size/1073741824, 1)." GB";}
 	  return $size; }
-
  	// Checks to see if veiwing hidden files is enabled
 	if($_SERVER['QUERY_STRING']=="hidden") { $hide="";
 	 $ahref="./";
@@ -130,6 +157,7 @@ if ($ColorScheme == '5') {
 			<td sorttable_customkey='$sizekey'><a href='./$namehref/.index.php'>$size</a></td>
 			<td sorttable_customkey='$timekey'><a href='./$namehref/.index.php'>$modtime</a></td>
 		</tr>"); } }
+// / -----------------------------------------------------------------------------------
 	?>
 	    </tbody>
 	</table>
