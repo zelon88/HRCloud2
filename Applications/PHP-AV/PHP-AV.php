ebc5810b653a1ce251a26cee15fbbd3f48e884a0
zelon88 <zelon88@gmail.com>
2017-04-04 00:19:43 -0400

    PHP-AV Defs to v3.0.
    -Fixed bugs in virus.def syntax.

--- Applications/PHP-AV/PHP-AV.php
+++ Applications/PHP-AV/PHP-AV.php
@@ -11,7 +11,7 @@ App Integration: 0 (False)
 App Permission: 0 (Admin)
 HRCLOUD2-PLUGIN-END
 //*/
-$versions = 'PHP-AV App v2.3.5 | Virus Definition v2.9, 4/3/2017';
+$versions = 'PHP-AV App v2.3.5 | Virus Definition v3.0, 4/4/2017';
 ?>
 <script type="text/javascript">
     function Clear() {    
