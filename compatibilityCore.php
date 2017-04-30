d06c577220e3a29e7ca0a1127709483ef5d641d8
zelon88 <zelon88@gmail.com>
2017-04-29 22:55:53 -0400

    v1.5 - Update 6x Apps. Add 1x App`.
    -v1.5.
    -Update Teams App, Grabber App, Contacts App, Notes App, ServStat App, and ServMonitor App.
    -Added a ton of App icons and Icon_Credits.
    -Working on Teams App logic.
    -Improved the logic of Notes and Contacts Apps to avoid errors on new-installations.
    -Added Bookmarks App to manage and store user supplied Bookark URL's on the HRC2 server.

--- compatibilityCore.php
+++ compatibilityCore.php
@@ -2,8 +2,8 @@
 
 /*
 HRCLOUD2 VERSION INFORMATION
-THIS VERSION : v1.4.9
-WRITTEN ON : 4/25/2017
+THIS VERSION : v1.5
+WRITTEN ON : 4/29/2017
 */
 
 // / -----------------------------------------------------------------------------------
