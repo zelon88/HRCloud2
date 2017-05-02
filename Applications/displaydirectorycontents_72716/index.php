0b1c3d74f2a9307a606916371ee99e7fbb3c5365
zelon88 <zelon88@gmail.com>
2017-05-01 23:28:23 -0400

    Refactored code. Improved perf, re-added icons.
    -Cloud Drive items now show a file icon next to them! Someday maybe they'll show the filetype like they used to *cough in alpha *cough*cough (walks-away).

--- Applications/displaydirectorycontents_72716/index.php
+++ Applications/displaydirectorycontents_72716/index.php
@@ -10,18 +10,13 @@ function goBack() {
    <link rel="shortcut icon" href="Applications/displaydirectorycontents_72716/favicon.ico">
    <title>Cloud Contents</title>
 <?php
-// / Verify the config.php file.
-if (!file_exists('/var/www/html/HRProprietary/HRCloud2/config.php')) {
-  echo nl2br('</head>ERROR!!! HRC2Index19, Cannot process the HRCloud2 configuration file (config.php).'."\n"); 
+// / The follwoing code checks if the commonCore.php file exists and 
+// / terminates if it does not.
+if (!file_exists('/var/www/html/HRProprietary/HRCloud2/commonCore.php')) {
+  echo nl2br('</head><body>ERROR!!! HRC2TeamsApp35, Cannot process the HRCloud2 Common Core file (commonCore.php)!'."\n".'</body></html>'); 
   die (); }
 else {
-  require('/var/www/html/HRProprietary/HRCloud2/config.php'); }
-$WPFile = '/var/www/html/wp-load.php';
-// / Verify that WordPress is installed.
-if (!file_exists($WPFile)) {
-  echo nl2br('</head>ERROR!!! HRC2Index27, WordPress was not detected on the server.'."\n"); }
-  else {
-    require_once($WPFile); } 
+  require_once ('/var/www/html/HRProprietary/HRCloud2/commonCore.php'); }
 // / Set all primary array data.
 $PLMediaArr =  array('mp2', 'mp3', 'wma', 'wav', 'aac', 'flac', 'ogg', 'avi', 'mov', 'mkv', 'flv', 'ogv', 'wmv', 'mpg', 'mpeg', 'm4v', '3gp', 'mp4');
 $ArchiveArray = array('zip', 'rar', 'tar', 'bz', 'gz', 'bz2', '7z', 'iso', 'vhd', 'vdi');
@@ -43,22 +38,15 @@ $ConvertInc = 0;
 $RenameInc = 0;
 $ConvertInc = 0;
 $EditInc = 0;
-// / Reset and re-detect the UserID, just-in-case.
-$UserIDRAW = get_current_user_id();
-$UserID = hash('ripemd160', $UserIDRAW.$Salts);
-// / Handle integrated App variables.
-$UserContacts = $InstLoc.'/DATA/'.$UserID.'/.AppData/.contacts.php';
-$UserSharedIndex = $URL.'/HRProprietary/HRCloud2/DATA/'.$UserID.'/.AppData/Shared/.index.php';
-$UserNotes = $InstLoc.'/DATA/'.$UserID.'/.AppData/.notes.php';
-$UserConfig = $InstLoc.'/DATA/'.$UserID.'/.AppData/.config.php';
-// / User config loader.
-if (!file_exists($UserConfig)) {
-  @chmod($UserConfig, 0755); }
-if (!file_exists($UserConfig)) {
-  echo nl2br('</head>ERROR!!! HRC2Index35, User Cache file was not detected on the server!'."\n"); 
-  die (); }
-else {
-    require($UserConfig); } 
+
+// / GUI specific resources.
+  function pretty_filesize($file) {
+    $size=filesize($file);
+    if($size<1024){$size=$size." Bytes"; }
+    elseif(($size<1048576)&&($size>1023)){$size=round($size/1024, 1)." KB"; }
+    elseif(($size<1073741824)&&($size>1048575)){$size=round($size/1048576, 1)." MB"; }
+    else{$size=round($size/1073741824, 1)." GB"; }
+    return $size; }
 // / Color scheme handler.
 if ($ColorScheme == '0' or $ColorScheme == '' or !isset($ColorScheme)) {
   $ColorScheme = '1'; }
@@ -72,32 +60,6 @@ if ($ColorScheme == '4') {
   echo ('<link rel="stylesheet" type="text/css" href="Applications/displaydirectorycontents_72716/styleGREY.css">'); }
 if ($ColorScheme == '5') {
   echo ('<link rel="stylesheet" type="text/css" href="Applications/displaydirectorycontents_72716/styleBLACK.css">'); } 
-// / User directory handler.
-if (isset($_POST['UserDir']) or isset($_POST['UserDirPOST'])) {
-  if ($_POST['UserDir'] == '/' or $_POST['UserDirPOST'] == '/') { 
-    $_POST['UserDir'] = '/'; 
-    $_POST['UserDirPOST'] = '/'; } 
-  $Udir = $_POST['UserDirPOST'].'/'; }
-if (!isset($_POST['UserDir']) or !isset($_POST['UserDirPOST'])) { 
-  $Udir = '/'; }
-if ($Udir == '//') {
-  $Udir = '/'; }
-if ($Udir == '//') {
-  $Udir = '/'; }
-if ($Udir == '//') {
-  $Udir = '/'; }
-// / User directory cleaner.
-$Udir = str_replace('//', '/', str_replace('//', '/', str_replace('//', '/', $Udir))); 
-$Udir = ltrim(rtrim($Udir,'//'),'/').'/';
-$Udir = str_replace('//', '/', str_replace('//', '/', str_replace('//', '/', $Udir))); 
-// / GUI specific resources.
-  function pretty_filesize($file) {
-    $size=filesize($file);
-    if($size<1024){$size=$size." Bytes"; }
-    elseif(($size<1048576)&&($size>1023)){$size=round($size/1024, 1)." KB"; }
-    elseif(($size<1073741824)&&($size>1048575)){$size=round($size/1048576, 1)." MB"; }
-    else{$size=round($size/1073741824, 1)." GB"; }
-    return $size; }
 // Checks to see if veiwing hidden files is enabled
 if ($_SERVER['QUERY_STRING']=="hidden") { 
   $hide="";
@@ -365,10 +327,6 @@ Are you sure?
           $extn = "Playlist"; } 
         $size = pretty_filesize($CloudUsrDir.$dirArray[$index]);
         $sizekey = filesize($CloudUsrDir.$dirArray[$index]); }
-$CleanUdir = str_replace('//', '/', $Udir.$name);
-$CleanUdir = str_replace('//', '/', $CleanUdir);
-$CleanUdir = str_replace('//', '/', $CleanUdir);
-$CleanDir = rtrim($CleanUdir, '/');
 if ($extn == 'HTML File' or $extn == 'PHP File' or $extn == 'CSS File') continue;
 $FileURL = 'DATA/'.$UserID.$UserDirPOST.$namehref;
 $extnRAW = pathinfo($dirArray[$index], PATHINFO_EXTENSION);
