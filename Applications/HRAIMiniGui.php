4197539bde9d5907a2a27b2fda440fa235c2d521
zelon88 <zelon88@gmail.com>
2017-05-03 00:33:29 -0400

    v1.5.2.9 - Moved HRAIMiniGUI to /Applications
    -v1.5.2.9.
    -Moved HRAIMiniGui to /Applications.

---
+++ Applications/HRAIMiniGui.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HRCloud2 | HRAIMini </title>
+</head>
+<body>
+  <?php 
+if (!isset($noMINICore)) {
+  $noMINICore = '0'; }
+
+$includeMINIIframerURL = 'core.php#end';  
+if (!isset($includeMINIIframer)) {
+  $includeMINIIframer = '1';
+  $includeMINIIframerURL = 'Applications/HRAI/core.php#end'; }
+
+if ($noMINICore == '0') {
+  require('Applications/HRAI/core.php'); }
+if ($includeMINIIframer == '1') { ?>
+  <div align="center" id='HRAIButtons2' name='HRAIButtons2'>
+  <hr />
+  <form action="<?php echo $includeMINIIframerURL; ?>" id="Corefile Input" method="post">
+  <input type="hidden" name="HRAIMiniGUIPost" value="1">
+  <?php if (!isset($input)) {
+    $input = ''; } ?>
+  <input type="text" name="input" id="input"  value="<?php echo $input; ?>">
+  <input id='submitHRAI' type="submit" value="Hello HRAI">
+  <input type="hidden" name="display_name" value="<?php echo $display_name;?>"></form>
+  </div>
+<?php } ?>
+
+</body>
+</html>
