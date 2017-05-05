df4502d1bb0da9a9cd10b05804273a9e077a60e7
zelon88 <zelon88@gmail.com>
2017-05-05 00:12:30 -0400

    v1.5.3 - Working on API Core.
    -Fixed HRAI.
    -Working on API Core.
    -Check out the zelon88/HRCloud2-Client repo to see a useless pre-Alpha of the standalone, multi-platform desktop client!

--- apiCore.php
+++ apiCore.php
@@ -1,12 +1,47 @@
 <?php
 
+// / -----------------------------------------------------------------------------------
+// / The follwoing code checks if the required core files exists, and terminates if they do not.
+if (!file_exists('sanitizeCore.php')) {
+  echo nl2br('</head><body>ERROR!!! HRC2Index1-10, Cannot process the HRCloud2 Sanitization Core file (sanitizeCore.php)!'."\n".'</body></html>'); 
+  die (); }
+else {
+  require('sanitizeCore.php'); }
+if (!file_exists('commonCore.php')) {
+  echo nl2br('</head><body>ERROR!!! HRC2Index1-19, Cannot process the HRCloud2 Common Core file (commonCore.php)!'."\n".'</body></html>'); 
+  die (); }
+else {
+  require('commonCore.php'); }
+if (!file_exists('securityCore.php')) {
+  echo nl2br('</head><body>ERROR!!! HRC2Index1-19, Cannot process the HRCloud2 Security Core file (securityCore.php)!'."\n".'</body></html>'); 
+  die (); }
+else {
+  require('securityCore.php'); } 
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / The following code sets the global variables for the session.
+$ServerKeyDir = $CloudLoc.'/Keys/'.$ServerID;
+$ServerKeyFile = $ServerKeyDir.'/'.$ServerID.'.key';
+$UserKey = $Salts.$UserID;
+$UserKeyDir = $CloudDir.'/Keys';
+$UserKeyFile = $UserKeyDir.'/'.$UserID.'/key';
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+
+
 // / Add sudo apt-get install ssh to the readme.md file and dependency requirements.
 
 // / A settings entry needs to be added to settingsCore.php giving admins the ability to enable global SFTP. 
 
 // / A settings entry needs to be added to settingsCore.php giving users the ability to enable/disable their private SFTP (for security).
 
+
+
 // / This file should check for the existence of a User API Key, and create one if one does not exist and if global and private SFTP are enabled.
+
+
 
 // / This file should provide the user a means to view/refresh their API Key.
 
