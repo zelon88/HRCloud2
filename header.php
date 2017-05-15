8761b7de92ff23d6a026057e7dcb3646d2d982fc
zelon88 <zelon88@gmail.com>
2017-05-14 20:09:22 -0400

    Sync Files.
    Sync header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <div class="nav">
       <ul>
         <li class="Cloud"><a href="index1.php">Cloud</a></li>
-        <li class="Aoos"><a href="appLauncher.php">Apps</a></li>
+        <li class="Apps"><a href="appLauncher.php">Apps</a></li>
         <li class="Drive"><a href="index2.php">Drive</a></li>
         <li class="Settings"><a href="settings.php"> Settings</a></li>
         <li class="Logs"><a href="logs.php">Logs</a></li>
