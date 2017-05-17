12d5677d48a7cb42901d6aec00aec111e32e40dd
zelon88 <zelon88@gmail.com>
2017-05-17 00:15:10 -0400

    PHP-AV A/V Defs to v3.3
    -PHP-AV A/V Defs to v3.3.
    -Added detection for more Hanictor (no, Hanictor will never end), Mordor, Bunitu, variious Malspam and more...
    -Very distracted today. Very bad day. Lets hope good days are ahead. I sure could use them.

--- Applications/PHP-AV/PHP-AV.php
+++ Applications/PHP-AV/PHP-AV.php
@@ -11,7 +11,7 @@ App Integration: 0 (False)
 App Permission: 0 (Admin)
 HRCLOUD2-PLUGIN-END
 //*/
-$versions = 'PHP-AV App v2.3.7 | Virus Definition v3.2, 5/15/2017';
+$versions = 'PHP-AV App v2.3.7 | Virus Definition v3.3, 5/16/2017';
 ?>
 <script type="text/javascript">
     function Clear() {    
