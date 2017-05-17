f9a904d1ee7177946199071b41df9a5ac5ba45c5
zelon88 <zelon88@gmail.com>
2017-05-17 00:17:05 -0400

    Teams App to v0.8.2.6.
    -Sync files.

---
+++ Applications/Teams/_SCRIPTS/friendsSidebar.php
@@ -0,0 +1,28 @@
+<?php
+  // / The following code represents the "All Teams" sidebar section.
+  echo ('<div class=\'sidebar\'>
+    <div id=\'teamsSidebarDiv\' style=\'display:none;\' class=\'sidebar-content\'>');
+  $publicTeams = getPublicTeamsQuietly($teamsList);
+  $publicTeamCounter = count($publicTeams);
+  $publicTeamCounter1 = 0;
+  echo ('<a href=\'?showTeams=1\'><strong>All Teams</strong></a>');
+  foreach ($publicTeams as $publicTeam) { 
+  if ($publicTeamCounter1 >= $publicTeamCounter or $publicTeamCounter == 0 or $publicTeam['name'] == '') continue;
+    echo ('<a href=\'?joinTeam='.$publicTeam['id'].'\'><'.$publicTeam['name'].'</a>'); 
+    echo ('<a href=\'?joinTeam='.$publicTeam['id'].'\'><i>'.$publicTeam['description'].'</i></a>'); 
+    $publicTeamCounter1++; } 
+  if ($publicTeamCounter == 0 or $publicTeamCounter1 == 0) {
+    echo ('<a href=\'?joinTeam=null\'>Nothing to show!</a>'); }
+  // / The following code represents the "My Teams" sidebar section.
+  $myTeams = getMyTeamsQuietly($teamsList);
+  $myTeamCounter = count($myTeams);
+  $myTeamCounter1 = 0;
+  echo ('<a href=\'?showTeams=1\'><strong>My Teams</strong></a>');
+  foreach ($myTeams as $myTeam) { 
+  if ($myTeamCounter1 >= $myTeamCounter or $myTeamCounter == 0 or $myTeam['name'] == '') continue;
+    echo ('<a href=\'?joinTeam='.$myTeam['id'].'\'>'.$myTeam['name'].'</a>'); 
+    echo ('<a href=\'?joinTeam='.$myTeam['id'].'\'><i>'.$myTeam['description'].'</i></a>'); 
+    $myTeamCounter1++; }
+  if ($myTeamCounter == 0 or $myTeamCounter1 == 0) {
+    echo ('<a href=\'?joinTeam=null\'>Nothing to show!</a>'); } 
+  echo('</div></div>'); 
