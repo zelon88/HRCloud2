0e918cbb0c3136daf1782ae0ab21f948fd62ca57
zelon88 <zelon88@gmail.com>
2017-05-21 23:29:35 -0400

    PHP-AV Virus defs to v3.4.
    -PHP-AV virus defs to v3.4.
    -Added about 50x or so Blank Slate variants. 
    -Added latest RigEK.Bunitu variant.
    -Fixed some whitespace.

--- Applications/PHP-AV/PHP-AV.php
+++ Applications/PHP-AV/PHP-AV.php
@@ -11,7 +11,7 @@ App Integration: 0 (False)
 App Permission: 0 (Admin)
 HRCLOUD2-PLUGIN-END
 //*/
-$versions = 'PHP-AV App v2.3.7 | Virus Definition v3.3, 5/16/2017';
+$versions = 'PHP-AV App v2.3.7 | Virus Definition v3.4, 5/21/2017';
 ?>
 <script type="text/javascript">
     function Clear() {    
