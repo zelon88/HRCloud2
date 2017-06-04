2707153a42402b78c85615fb516aabeca5654ed2
zelon88 <zelon88@gmail.com>
2017-06-04 12:50:54 -0400

    Teams App to v0.8.4
    -Team creation is functional.
    -Working on displaying Teams properly in the sidebar.
    -Making progress.

--- Applications/Teams/_SCRIPTS/teamsSidebar.php
+++ Applications/Teams/_SCRIPTS/teamsSidebar.php
@@ -1,24 +1,26 @@
 <?php
   // / The following code represents the "All Teams" sidebar section.
   echo (' <div id=\'teamsSidebarDiv\' style=\'display:none;\' class=\'sidebar-content\'>');
-  $publicTeams = getPublicTeamsQuietly($teamsList);
+  $publicTeams = $teamsList;
   $publicTeamCounter = count($publicTeams);
   $publicTeamCounter1 = 0;
   echo ('<a href=\'?showTeams=1\'><strong>All Teams</strong></a>');
-  foreach ($publicTeams as $publicTeam) { 
-  if ($publicTeamCounter1 >= $publicTeamCounter or $publicTeamCounter == 0 or $publicTeam['name'] == '') continue;
-    echo ('<a href=\'?joinTeam='.$publicTeam['id'].'\'><'.$publicTeam['name'].'</a>'); 
-    echo ('<a href=\'?joinTeam='.$publicTeam['id'].'\'><i>'.$publicTeam['description'].'</i></a>'); 
+  foreach ($teamsList as $publicTeam) { 
+    if ($publicTeamCounter1 >= $publicTeamCounter or in_array($publicTeam, $defaultDirs)) continue;
+    include ($TeamsDir.'/'.$publicTeam.'/'.$publicTeam.'_DATA.php');
+    echo ('<a href=\'?joinTeam='.$publicTeam.'\'><'.$TEAM_NAME.'</a>'); 
+    echo ('<a href=\'?joinTeam='.$publicTeam.'\'><i>'.$TEAM_DESCRIPTION.'</i></a>'); 
     $publicTeamCounter1++; } 
   if ($publicTeamCounter == 0 or $publicTeamCounter1 == 0) {
     echo ('<a href=\'?joinTeam=view\'>Nothing to show!</a>'); }
+
   // / The following code represents the "My Teams" sidebar section.
-  $myTeams = getMyTeamsQuietly($teamsList);
+  $myTeams = getMyTeamsQuietly($myTeamsList);
   $myTeamCounter = count($myTeams);
   $myTeamCounter1 = 0;
   echo ('<a href=\'?showTeams=1\'><strong>My Teams</strong></a>');
   foreach ($myTeams as $myTeam) { 
-  if ($myTeamCounter1 >= $myTeamCounter or $myTeamCounter == 0 or $myTeam['name'] == '') continue;
+    if ($myTeamCounter1 >= $myTeamCounter or $myTeamCounter == 0 or $myTeam['name'] == '') continue;
     echo ('<a href=\'?joinTeam='.$myTeam['id'].'\'>'.$myTeam['name'].'</a>'); 
     echo ('<a href=\'?joinTeam='.$myTeam['id'].'\'><i>'.$myTeam['description'].'</i></a>'); 
     $myTeamCounter1++; }
