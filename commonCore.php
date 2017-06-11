16dc09426e5783b59a97739785b5a4d12083844c
zelon88 <zelon88@gmail.com>
2017-06-11 14:08:25 -0400

    v1.5.6.7 - Improve Gzip support.
    -v1.5.6.7.
    -Basically undo the last commit. 
    -Removing the Gzip handler improves support regardless of if Gzip is enabled in Apache or not.
    -Fix typo in appCore.php (install App is now Install App).

--- commonCore.php
+++ commonCore.php
@@ -4,8 +4,6 @@
 // / file, authenticate user storage, and define variables for the session.
 // / -----------------------------------------------------------------------------------
 
-ob_start("ob_gzhandler");
-
 // / -----------------------------------------------------------------------------------
 // / The follwoing code checks if the sanitizeCore.php file exists and 
 // / terminates if it does not.
