ac9b7bab9c73f88acdde589d4543934e2506a605
zelon88 <zelon88@gmail.com>
2017-06-19 20:42:24 -0400

    v1.5.7 - PHP-AV App A/V Defs to v3.9.
    -v1.5.7.
    -PHP-AV App A/V Defs to v3.9.

--- compatibilityCore.php
+++ compatibilityCore.php
@@ -2,8 +2,8 @@
 
 /*
 HRCLOUD2 VERSION INFORMATION
-THIS VERSION : v1.5.6.9
-WRITTEN ON : 6/11/2017
+THIS VERSION : v1.5.7
+WRITTEN ON : 6/19/2017
 */
 
 // / -----------------------------------------------------------------------------------
