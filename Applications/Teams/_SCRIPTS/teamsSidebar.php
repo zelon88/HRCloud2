8c4f419e9fdb8d26848299bcfb7b1e73222ff617
zelon88 <zelon88@gmail.com>
2017-06-21 18:53:03 -0400

    v0.8.6 - Working on sidebars some more.
    -v0.8.6.
    -Working on sidebars some more.

--- Applications/Teams/_SCRIPTS/teamsSidebar.php
+++ Applications/Teams/_SCRIPTS/teamsSidebar.php
@@ -4,13 +4,17 @@
   $publicTeams = $teamsList;
   $publicTeamCounter = count($publicTeams);
   $publicTeamCounter1 = 0;
+  $lastPublicTeam = 0;
   echo ('<a href=\'?showTeams=1\'><strong>All Teams</strong></a>');
   foreach ($teamsList as $publicTeam) { 
     if ($publicTeamCounter1 >= $publicTeamCounter or in_array($publicTeam, $defaultDirs)) continue;
+    $publicTeamFile = $TeamsDir.'/'.$publicTeam.'/'.$publicTeam.'_DATA.php';
+    if (!file_exists($publicTeamFile)) continue;    
     include ($TeamsDir.'/'.$publicTeam.'/'.$publicTeam.'_DATA.php');
-    echo ('<a href=\'?joinTeam='.$publicTeam.'\'><'.$TEAM_NAME.'</a>'); 
-    echo ('<a href=\'?joinTeam='.$publicTeam.'\'><i>'.$TEAM_DESCRIPTION.'</i></a>'); 
-    $publicTeamCounter1++; } 
+    if ($TEAM_NAME == '') continue;
+    echo ('<a href=\'?joinTeam='.$publicTeam.'\'>'.$TEAM_NAME.'</a>'); 
+    $publicTeamCounter1++; 
+    $lastPublicTeam = $publicTeam; } 
   if ($publicTeamCounter == 0 or $publicTeamCounter1 == 0) {
     echo ('<a href=\'?joinTeam=view\'>Nothing to show!</a>'); }
 
@@ -18,12 +22,17 @@
   $myTeams = getMyTeamsQuietly($myTeamsList);
   $myTeamCounter = count($myTeams);
   $myTeamCounter1 = 0;
+  $lastMyTeam = 0;
   echo ('<a href=\'?showTeams=1\'><strong>My Teams</strong></a>');
   foreach ($myTeams as $myTeam) { 
-    if ($myTeamCounter1 >= $myTeamCounter or $myTeamCounter == 0 or $myTeam['name'] == '') continue;
-    echo ('<a href=\'?joinTeam='.$myTeam['id'].'\'>'.$myTeam['name'].'</a>'); 
-    echo ('<a href=\'?joinTeam='.$myTeam['id'].'\'><i>'.$myTeam['description'].'</i></a>'); 
-    $myTeamCounter1++; }
+    if ($myTeamCounter1 >= $myTeamCounter or $myTeamCounter == 0 or in_array($myTeam, $defaultDirs)) continue;
+    $myTeamFile = $TeamsDir.'/'.$myTeam.'/'.$myTeam.'_DATA.php';
+    if (!file_exists($myTeamFile)) continue;
+    include ($myTeamFile);
+    if ($TEAM_NAME == '') continue;
+    echo ('<a href=\'?joinTeam='.$myTeam.'\'>'.$TEAM_NAME.'</a>'); 
+    $myTeamCounter1++; 
+    $lastMyTeam = $myTeam; }
   if ($myTeamCounter == 0 or $myTeamCounter1 == 0) {
     echo ('<a href=\'?joinTeam=view\'>Nothing to show!</a>'); } 
   echo('</div>'); 
