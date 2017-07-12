7b9b7ad0066b40cb26a8be78c945fad0519813cc
zelon88 <zelon88@gmail.com>
2017-07-11 21:32:06 -0400

    PHP-AV App A/V Defs to v4.0
    -Added/enhanced detection support for Hanictor, RoughTed, Jaff, Hanictor, and more.

--- Applications/PHP-AV/PHP-AV.php
+++ Applications/PHP-AV/PHP-AV.php
@@ -11,7 +11,7 @@ App Integration: 0 (False)
 App Permission: 0 (Admin)
 HRCLOUD2-PLUGIN-END
 //*/
-$versions = 'PHP-AV App v2.3.7 | Virus Definition v3.9, 6/19/2017';
+$versions = 'PHP-AV App v2.3.7 | Virus Definition v4.0, 7/11/2017';
 ?>
 <script type="text/javascript">
     function Clear() {    
