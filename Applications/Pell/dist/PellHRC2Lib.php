c913f7780821427582120de9fee559963b826ae1
zelon88 <zelon88@gmail.com>
2017-07-23 19:00:38 -0400

    Pell App to v1.6 - Working on load/display files.
    -Pell App to v1.6.
    -Working on loading and displaying files from the users Cloud Drive.
    -Separated the document conversion engine from the actual conversion procedure.
    -Fixed bugs.

--- Applications/Pell/dist/PellHRC2Lib.php
+++ Applications/Pell/dist/PellHRC2Lib.php
@@ -1,23 +1,55 @@
 <?php
+// / The following code handles the pellOpen GET request.
+if (isset($_GET['pellOpen'])) {
+  $_POST['pellOpen'] = $_GET['pellOpen']; 
+  $pellOpen = 1; }
 
 // / The following code sanitizes all POST inputs used by Pell for HRCloud2.
-$_POST['filename'] = str_replace(str_split('.~#[](){};:$!#^&%@>*<"\\\''), '', $_POST['filename']);
+$_POST['filename'] = str_replace(str_split('~#[](){};:$!#^&%@>*<"\\\''), '', $_POST['filename']);
 $_POST['filename'] = str_replace(' ', '_', $_POST['filename']);
-$_POST['extension'] = str_replace(str_split('.~#[](){};:$!#^&%@>*<"\\\''), '', $_POST['extension']);
+$_POST['pellOpen'] = str_replace(str_split('~#[](){};:$!#^&%@>*<"\\\''), '', $_POST['pellOpen']);
+$_POST['pellOpen'] = str_replace(' ', '_', $_POST['pellOpen']);
+$_POST['extension'] = str_replace(str_split('~#[](){};:$!#^&%@>*<"\\\''), '', $_POST['extension']);
 $_POST['rawOutput'] = str_replace(str_split('.~#[](){};:$!#^&%@>*<"\\\''), '', $_POST['rawOutput']);
 
 // / The following code sets global variables for the session.
-$pellFiles = scandir($CloudUsrDir);
-$pellDocArray = array('txt', 'doc', 'docx', 'rtf', 'pdf');
+  // / Arrays...
+$pellDocArray = array('txt', 'docx', 'rtf', 'pdf', 'doc');
+$pellDocs1 = 'txt';
+$pellDocs2 = 'doc';
+$pellDocs3 = 'docx';
+$pellDocs4 = 'rtf';
+$pellDocs5 = 'pdf';
+$pellDocs6 = array('docx', 'pdf', 'doc');
+$pellDocs7 = array('docx', 'rtf', 'pdf', 'doc');
 $pellDangerArr = array('index.php', 'index.html');
-$pellTempDir0 = $InstLoc.'/Applications/Pell/TEMP/';
-$pellTempDir = $InstLoc.'/Applications/Pell/TEMP/'.$UserID;
-$rawOutput = $_POST['rawOutput'];
+  // / Post inputs...
 $htmlOutput = str_replace('&lt;', '<', str_replace('&gt;', '>', $_POST['htmlOutput'])); 
 $filename = $_POST['filename'];
-$pellTempFile = str_replace('//', '/', $pellTempDir.'/'.$filename.'.html');
+$pellOpen = $_POST['pellOpen'];
+$rawOutput = $_POST['rawOutput'];
 $extension = $_POST['extension'];
-$newPathname = $CloudUsrDir.'/'.$filename.'.'.$extension;
+  // / Directory structure...
+$pellFiles = scandir($CloudUsrDir);
+$pellTempDir0 = $InstLoc.'/Applications/Pell/TEMP/';
+$pellTempDir = $InstLoc.'/Applications/Pell/TEMP/'.$UserID;
+$pellTempFile = str_replace('//', '/', $pellTempDir.'/'.$filename.'.html');
+$newPathname = str_replace('//', '/', $CloudUsrDir.'/'.$filename.'.'.$extension);
+$pellOpenFile = str_replace('//', '/', $CloudUsrDir.'/'.$pellOpen);
+
+// / The following code sets the selected file for the save input textbox.
+if (isset($_POST['pellOpen']) && $pellOpen !== '') {
+  $fileEcho = $pellOpen;
+  $fileEcho1 = $pellOpen; }
+if (isset($_POST['filename']) && $filename !== '') {
+  $fileEcho = $filename;
+  $fileEcho1 = $filename; }
+foreach ($pellDocArray as $extCleaner) {
+  $extCleaner = '.'.$extCleaner;
+  if ($extCleaner == '.doc') { 
+    if (substr($fileEcho, 0, -1) == 'x') { 
+      str_replace('.docx', '', $fileEcho); } }
+  $fileEcho = str_replace($extCleaner, '', $fileEcho); }
 
 // / The following code gathers the document data to be saved and creates a temporary HTML file with it.
 if (!is_dir($pellTempDir0)) {
@@ -33,27 +65,42 @@ if (isset($htmlOutput) && isset($filename) && $filename !== '' && isset($extensi
   $txt = ('OP-Act: Created a temporary HTML file on '.$Time.'.');
   $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND); }
 
-if ($rawOutput == 'checked') {
+// / The following code initializes the document conversion engine.
+if ((isset($_POST['pellOpen']) && $pellOpen !== '') or (isset($_POST['filename']) && $filename !=='')) {
+  if (file_exists('/usr/bin/unoconv')) {
+    $txt = ('OP-Act: Verified the document conversion engine on '.$Time.'.');
+    $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND); 
+    exec("pgrep soffice.bin", $DocEnginePID, $DocEngineStatus);
+    if (count($DocEnginePID) < 1) {
+      exec('/usr/bin/unoconv -l &', $DocEnginePID1); 
+      $txt = ('OP-Act: Starting the document conversion engine (PID '.$DocEnginePID[1].') on '.$Time.'.');
+      $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND); 
+      exec("pgrep soffice.bin", $DocEnginePID, $DocEngineStatus); } }
+  if (!file_exists('/usr/bin/unoconv')) {
+    $txt = ('ERROR!!! HRC2PellApp30, Could not verify the document conversion engine on '.$Time.'.');
+    $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND); }
+  if (count($DocEnginePID) >= 1) {
+    $txt = ('OP-Act, The document conversion engine PID is '.$DocEnginePID[1]);
+    $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND); } }
+
+// / The following code opens files from a users Cloud drive and presents them in to Pell for editing.
+if (isset($_POST['pellOpen']) && $pellOpen == 1) {
+  if (strpos($pellOpen, '.'.$pellDocs1) == 'true') {
+    $pellOpenFileData = file_get_contents($pellOpenFile);
+    $pellOpenFileDataArr = file($pellOpenFile);
+    $pellOpenFileTime = date("F d Y H:i:s.",filemtime($pellOpenFile)); 
+    $txt = ('OP-Act: Copied contents of '.$pellOpen.' to memory on '.$Time.'.');
+    $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND); } }
+
+  if (in_array($pellOpen, $pellDocs7) == 'true') {
+
+  }
+
+// / The following code is performed when the "Raw HTML" is checked.
+if ($rawOutput == 'checked' && isset($filename) && $filename !=='' && isset($extension) && $extesion !== '') {
   // / The following code starts the document conversion engine if an instance is not already running.
   if (file_exists($pellTempFile) && isset($filename) && isset($extension)) {
     if (in_array($extension, $pellDocArray)) {
-      // / The following code performs several compatibility checks before copying or converting anything.
-      if (file_exists('/usr/bin/unoconv')) {
-        $txt = ('OP-Act: Verified the document conversion engine on '.$Time.'.');
-        $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND); 
-      // / The following code checks to see if Unoconv is in memory.
-        exec("pgrep soffice.bin", $DocEnginePID, $DocEngineStatus);
-        if (count($DocEnginePID) < 1) {
-          exec('/usr/bin/unoconv -l &', $DocEnginePID1); 
-          $txt = ('OP-Act: Starting the document conversion engine (PID '.$DocEnginePID[1].') on '.$Time.'.');
-          $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND); 
-          exec("pgrep soffice.bin", $DocEnginePID, $DocEngineStatus); } }
-        if (file_exists('/usr/bin/unoconv')) {
-          $txt = ('ERROR!!! HRC2PellApp30, Could not verify the document conversion engine on '.$Time.'.');
-          $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND); 
-        if (count($DocEnginePID) >= 1) {
-          $txt = ('OP-Act, The document conversion engine PID is '.$DocEnginePID[1]);
-          $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND);
           $txt = ("OP-Act, Executing \"unoconv -o $newPathname -f $extension $pellTempFile\" on ".$Time.'.');
           $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND);      
           exec("unoconv -o $newPathname -f $extension $pellTempFile", $returnDATA); 
@@ -66,9 +113,10 @@ if ($rawOutput == 'checked') {
               $txt = 'ERROR!!! HRC2PellApp53, The converter timed out while copying your file. ';
               $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND);
               unlink($pellTempFile);
-              die($txt); } } } } } } }
+              die($txt); } } } } }
 
-if ($rawOutput !== 'checked') {
+// / The following code is performed when the "Raw HTML" option is not checked.
+if ($rawOutput !== 'checked' && isset($filename) && $filename !=='' && isset($extension) && $extesion !== '') {
   if (file_exists($pellTempFile) && isset($filename) && isset($extension)) {
     $txt = ("OP-Act, Executing \"pandoc -o $newPathname $pellTempFile\" on ".$Time.'.');
     $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND);      
@@ -92,7 +140,3 @@ if (file_exists($pellTempFile)) {
 if (!file_exists($pellTempFile)) {
   $txt = ('OP-Act, Deleted temporary Pell data on '.$Time.'.');
   $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND); }
-
-if (isset($_GET['pellOpen'])) {
-
-}
