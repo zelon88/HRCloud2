6aac23377c843483543532fef584bbadc6b114d5
zelon88 <zelon88@gmail.com>
2017-07-30 20:48:59 -0400

    PHP-AV App to v2.4 - Fixed insufficient memory bug
    -PHP-AV App to v2.4.
    -Fixed bug where PHP-AV would run out of memory and fail a scan if a file it was scanning exceeded the amount of available memory set in php-ini.
    -Now PHP-AV will skip files larger than it's memory allotment rather than crashing. 
    -Increasing the available memory in your servers php.ini file will allow PHP-AV to scan larger files.

--- Applications/PHP-AV/PHP-AV.php
+++ Applications/PHP-AV/PHP-AV.php
@@ -1,17 +1,25 @@
 <?php
-
+// / -----------------------------------------------------------------------------------
 /*//
 HRCLOUD2-PLUGIN-START
 App Name: PHP-AV
-App Version: 2.3.8 (7-19-2017 08:00)
+App Version: 2.4 (7-30-2017 08:00)
 App License: GPLv3
 App Author: FujitsuBoy (aka Keyboard Artist) & zelon88
 App Description: A simple HRCloud2 App for scanning files for viruses.
 App Integration: 0 (False)
 App Permission: 0 (Admin)
 HRCLOUD2-PLUGIN-END
+
+PHP ANTI-VIRUS v2.4
+Written by FujitsuBoy (aka Keyboard Artist)
+Modified by zelon88
 //*/
-$versions = 'PHP-AV App v2.3.7 | Virus Definition v4.0, 7/11/2017';
+$versions = 'PHP-AV App v2.4 | Virus Definition v4.0, 7/11/2017';
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / The following code represents the Javascript required for PHP-AV to function.
 ?>
 <script type="text/javascript">
     function Clear() {    
@@ -27,20 +35,29 @@ $versions = 'PHP-AV App v2.3.7 | Virus Definition v4.0, 7/11/2017';
     </script>
 <div align="center"><h3>PHP-AV</h3><hr /></div>
 <?php
-// PHP ANTI-VIRUS v2.3.8
-// Written by FujitsuBoy (aka Keyboard Artist)
-// Modified by zelon88
+// / -----------------------------------------------------------------------------------
 
+// / -----------------------------------------------------------------------------------
+// / The following code sets the memory limit for scanned files (larger files will be skipped).
+$memoryLimit = (rtrim(ini_get("memory_limit"). 'M') * 1024 * 1024);
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
 // / The following code loads needed HRCloud2 features and functions.
 require('/var/www/html/HRProprietary/HRCloud2/config.php');
 require('/var/www/html/HRProprietary/HRCloud2/commonCore.php');
+// / -----------------------------------------------------------------------------------
 
+// / -----------------------------------------------------------------------------------
 // / The following code checks if App permission is set to '1' and if the user is an administrator or not.
-
 if ($UserIDRAW !== 1) {
   $txt = ('ERROR!!! HRC2PHPAVApp28, A non-administrator attempted to execute the PHP-AV App on '.$Time.'!'); 
   $MAKELogFile = file_put_contents($LogFile, $txt.PHP_EOL, FILE_APPEND);  
   die($txt); } 
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / The following code is performed when a user selects to scan their server with PHP-AV.
 if (!isset($_POST['AVScan'])) { ?>
 <div align="center">
 <br>
@@ -59,9 +76,11 @@ if (!isset($_POST['AVScan'])) { ?>
 <br>
 <input type="submit" name="AVScan" id="AVScan" value="Scan Server" onclick="toggle_visibility('loading');"></form>
 <div align="center"><img src='Resources/logosmall.gif' id='loading' name='loading' style="display:none; max-width:64px; max-height:64px;"/></div>
-
 </div>
 <?php }
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
 if (isset($_POST['AVScan'])) {
   $CONFIG = Array();
   $CONFIG['debug'] = 0;
@@ -73,23 +92,27 @@ if (isset($_POST['AVScan'])) {
     $CONFIG['scanpath'] = str_replace(' ', '\ ', str_replace(str_split('[]{};:$!#^&%@>*<'), '', $_POST['AVScanTarget'])); }
   if (!isset($_POST['AVScanTarget']) or $_POST['AVScanTarget'] == '') {
     $CONFIG['scanpath'] = $_SERVER['DOCUMENT_ROOT']; }
+// / -----------------------------------------------------------------------------------
 
+// / -----------------------------------------------------------------------------------
+// / The following functions perform the loading of definitions as well as scanning and verification of files being scanned.
 function file_scan($folder, $defs, $debug) {
   // Hunts files/folders recursively for scannable items.
   $defData = hash_file('sha256', 'virus.def');
-  global $dircount, $report;
+  global $dircount, $report, $memoryLimit;
   $dircount++;
   if ($debug)
-	$report .= '<p class="d">Scanning folder $folder ...</p>';
-	if ($d = @dir($folder)) {
-	  while (false !== ($entry = $d->read())) {
-		$isdir = @is_dir($folder.'/'.$entry);
-		if (!$isdir and $entry!='.' and $entry!='..') {
-		  virus_check($folder.'/'.$entry, $defs, $debug, $defData); } 
-		elseif ($isdir  and $entry!='.' and $entry!='..') {
-		  file_scan($folder.'/'.$entry, $defs, $debug, $defData); } }
-	  $d->close(); } }
-
+    $report .= '<p class="d">Scanning folder $folder ...</p>';
+    if ($d = @dir($folder)) {
+      while (false !== ($entry = $d->read())) {
+        $isdir = @is_dir($folder.'/'.$entry);
+        if (!$isdir and $entry!='.' and $entry!='..') {
+          virus_check($folder.'/'.$entry, $defs, $debug, $defData); } 
+        elseif ($isdir  and $entry!='.' and $entry!='..') {
+          $filesize = @filesize($folder.'/'.$entry);
+          if ($filesize >= $memoryLimit or $filesize == '') continue; 
+          file_scan($folder.'/'.$entry, $defs, $debug, $defData); } }
+      $d->close(); } }
 function load_defs($file, $debug) {
   // Reads tab-delimited defs file.
   $defs = file($file);
@@ -101,7 +124,6 @@ function load_defs($file, $debug) {
 if ($debug)
   echo '<p>Loaded ' . sizeof($defs) . ' virus definitions</p>';
   return $defs; }
-
 function check_defs($file) {
   // Check for >755 perms on virus defs.
   clearstatcache();
@@ -110,41 +132,45 @@ function check_defs($file) {
 	return false;
   else
 	return true; }
-
 function virus_check($file, $defs, $debug, $defData) {
   // Hashes and checks files/folders for viruses against static virus defs.
-  global $filecount, $infected, $report, $CONFIG;
+  global $memoryLimit, $filecount, $infected, $report, $CONFIG;
 	$filecount++;
 	if ($file !== 'virus.def')
 	  if (file_exists($file)) { 
-	    $data = file($file);
-	    $data = implode('\r\n', $data);
-	    $data1 = md5_file($file);
-	    $data2 = hash_file('sha256', $file);
-	    if ($defData !== $data2) {
-	      $clean = 1;
-	      foreach ($defs as $virus) {
-		    if ($virus[1] !== '') {
-		      if (strpos($data, $virus[1])) {
+	  	$filesize = filesize($file);
+	  	if ($memoryLimit > $filesize) {  
+	      $data = file($file);
+	      $data = implode('\r\n', $data);
+	      $data1 = md5_file($file);
+	      $data2 = hash_file('sha256', $file);
+	      if ($defData !== $data2) {
+	        $clean = 1;
+	        foreach ($defs as $virus) {
+		      if ($virus[1] !== '') {
+		        if (strpos($data, $virus[1])) {
+			      // File matches virus defs.
+			      $report .= '<p class="r">Infected: ' . $file . ' (' . $virus[0] . ')</p>';
+			      $infected++;
+			      $clean = 0; } }
+		        if ($virus[2] !== '') {
+                  if (strpos($data1, $virus[2])) {
+			      // File matches virus defs.
+			      $report .= '<p class="r">Infected: ' . $file . ' (' . $virus[0] . ')</p>';
+			      $infected++;
+			      $clean = 0; } }
+		      if ($virus[3] !== '') {
+                if (strpos($data2, $virus[3])) {
 			    // File matches virus defs.
 			    $report .= '<p class="r">Infected: ' . $file . ' (' . $virus[0] . ')</p>';
 			    $infected++;
-			    $clean = 0; } }
-		      if ($virus[2] !== '') {
-                if (strpos($data1, $virus[2])) {
-			    // File matches virus defs.
-			    $report .= '<p class="r">Infected: ' . $file . ' (' . $virus[0] . ')</p>';
-			    $infected++;
-			    $clean = 0; } }
-		    if ($virus[3] !== '') {
-              if (strpos($data2, $virus[3])) {
-			  // File matches virus defs.
-			  $report .= '<p class="r">Infected: ' . $file . ' (' . $virus[0] . ')</p>';
-			  $infected++;
-			  $clean = 0; } } }
-	      if (($debug)&&($clean))
-		    $report .= '<p class="g">Clean: ' . $file . '</p>'; } } }
+			    $clean = 0; } } }
+	        if (($debug)&&($clean)) {
+		      $report .= '<p class="g">Clean: ' . $file . '</p>'; } } } } } 
+// / -----------------------------------------------------------------------------------
 
+// / -----------------------------------------------------------------------------------
+// / The fopllowing code displays the PHP-AV results page after a scan has completed.
 function renderhead() {
 ?>
 <html>
@@ -153,29 +179,23 @@ function renderhead() {
 <style type="text/css">
 h1 {
 	font-family: arial; }
-
 p {
 	font-family: arial;
 	padding: 0;
 	margin: 0;
 	font-size: 10px; }
-
 .g {
 	color: #009900; }
-
 .r {
 	color: #990000;
 	font-weight: bold; }
-
 .d {
 	color: #ccc; }
-
 #summary {
 	border: #333 solid 1px;
 	background: #f0efca;
 	padding: 10px;
 	margin: 10px; }
-
 #summary p {
 	font-size: 12px; }
 </style>
@@ -203,6 +223,10 @@ echo '<p class=r><strong>Infected files:</strong> ' . $infected . '</p>';
 echo '</div>';
 // Output full report.
 echo $report; } 
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / The following code represents the PHP-AV Footer, which displays version information.
 ?>
 <hr />
 <p style="text-align:center;">Loaded virus definition: <i><a href="virus.def"><?php echo $versions; ?></i></p>
