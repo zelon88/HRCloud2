51f15c715e804d9d3d22c873669496c7789bf059
zelon88 <zelon88@gmail.com>
2017-09-06 23:20:26 -0400

    v1.7.7-PHP-AV to v2.7, Defs to v4.1, HRAI to v4.9.
    HRAI to v4.9.
    -Fix bugs with HRAI's calculator coreCommand functions. 
    PHP-AV App to v2.7, A/V Defs to v4.1.
    -Changed the way PHP-AV detects and chuinks up large files.
    -Added memoryLimit and chunkSize variables to the config file (defaults are 40mb memory limit, 10mb chunk size).
    -Files larger than the memory limit are chunked into bytes according to the chunkSize.
    -Added detection for a bunch of RigEK, Mole, and Gryphon ransomware variants.

--- compatibilityCore.php
+++ compatibilityCore.php
@@ -2,8 +2,8 @@
 
 /*
 HRCLOUD2 VERSION INFORMATION
-THIS VERSION : v1.7.6
-WRITTEN ON : 8/19/2017
+THIS VERSION : v1.7.7
+WRITTEN ON : 9/6/2017
 */
 
 // / -----------------------------------------------------------------------------------
