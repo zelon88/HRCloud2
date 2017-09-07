2ec80b9cdf7b66824d9b8ef5163bb63e17ce3302
zelon88 <zelon88@gmail.com>
2017-09-06 20:30:42 -0400

    Update HRAI to v4.9 - Fix calculator commands.
    -HRAI to v4.9.
    -Update Calculator coreCommands.
    -Fix bugs and errors.

--- Applications/HRAI/CoreCommands/CMDcalculator.php
+++ Applications/HRAI/CoreCommands/CMDcalculator.php
@@ -2,28 +2,21 @@
 
 $CMDfile = $InstLoc.'/Applications/HRAI/CoreCommands/CMDcalculator.php'; 
 $inputMATCH = array('calculator', 'the sum of', 'quotient of', 'plus', 'add', 'subtract', 'minus', 'take away', 
-  'divided by', 'multiplied by', 'times', '+', '-', '/', '*');
+  'divided by', 'multiplied by', 'times', '\+', '\-', '\/', '\*', 'x', 'X');
 $CMDcounter++;
+$CMDinit[$CMDcounter] = 0;
 
-if (!isset($input)) {
-  $_POST['calculatorInput'] = $_POST['input'];
-  $calculatorInput = $_POST['calculatorInput'];
-  $input = ''; }
-
-$input = str_replace('   ',' ',$input);
-$input = str_replace('  ',' ',$input);
-$input = rtrim($input);
-$input = ltrim($input);
+if (isset($input)) {
+  foreach ($inputMATCH as $inputM1) {
+    if (preg_match('/'.$inputM1.'/', $input)) {
+      $CMDinit[$CMDcounter] = 1;
+      $input = preg_replace('/'.$inputM1.'/',' ',$input); } } } 
 
 // / This CMDcommand was copied from the HRC2 Calculator App on 8/8/2017.
-
-// / HRAI Specific Code.
-if (isset($calculatorInput) && $calculatorInput == '') {
-  echo ('There was no equation to calculate!'); }
-
-// / Copy/Pasta code from 8/8/2017 HRC2 Calculator App..... Word-for-word.
-if (isset($_POST['calculatorInput']) && $_POST['calculatorInput'] !== '') {
+if ($CMDinit[$CMDcounter] == 1) {
+  $calculatorInput = $_POST['input'];
   $calculatorInput = strtolower($calculatorInput);
+
   // / The following code defines a function originally written by Justin Cook...
   // / http://www.justin-cook.com/wp/2006/03/31/php-parse-a-string-between-two-strings/
     function get_string_between($string, $start, $end) {
@@ -33,16 +26,18 @@ if (isset($_POST['calculatorInput']) && $_POST['calculatorInput'] !== '') {
       $ini += strlen($start);
       $len = strpos($string, $end, $ini) - $ini;
       return substr($string, $ini, $len); } 
+
   // / The following code sets the global variables for the session.
-  $numbers = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+  $numbers = range(0, 9);
+  // / Note that the letter "x" is missing from the $letters array.
+  $letters = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','z');
   $basicFunctions = array('+', '-', 'x', 'X', '*', '/');
   $advancedFunctions = '%';
   $additionFunctions = '+';
   $subtractionFunctions = '-';
   $multicationFunctions = '*';
   $divisionFunctions = '/';
-  $calculatorInput = str_replace(str_split('[]{};:$!#&@>=<?,'), '', $calculatorInput);
-  $calculatorInput = strtolower($calculatorInput);
+  $calculatorInput = str_replace(str_split('[]{};:$!#&@>=<?\'",'), '', $calculatorInput);
   $calculatorInput = str_replace(str_split('xX'), '*', $calculatorInput);
   $calculatorInput = str_replace('multiplied by', '*', $calculatorInput);
   $calculatorInput = str_replace('plus', '+', $calculatorInput);
@@ -51,17 +46,17 @@ if (isset($_POST['calculatorInput']) && $_POST['calculatorInput'] !== '') {
   $calculatorInput = str_replace('times', '*', $calculatorInput);
   $calculatorInput = str_replace('divided by', '/', $calculatorInput);
   $calculatorInput = str_replace(str_split('abcdefghijklmnopqrstuvwyz'), '', $calculatorInput);
+  $calculatorInput = str_replace(str_split('ABCDEFGHIJKLMNOPQRSTUVWYZ'), '', $calculatorInput);  
   $calculatorInput = str_replace(' ', '', $calculatorInput);
-  $calculatorArguments = str_replace(str_split('[]{};:$!#&@><'), '', $calculatorInput);
+
   // / The following code cleans the user input for the math interpreter.
   $counter = 0; 
   foreach ($numbers as $number) {
     $calculatorInput = str_replace($number.'(', $number.'*(', $calculatorInput);
     $calculatorInput = str_replace(')'.$number, ')*'.$number, $calculatorInput); }
-  if ($calculatorInput == '') {
-    echo ('There was no equation to calculate!'); }
   $priorityFunctions = get_string_between($calculatorInput, '(', ')');
   $priorityFunctions = str_replace(str_split('()'), '', $priorityFunctions);
+
   // / The following code prepares ONLY ONE (the first encountered) nested equation before proceeding.  
   if ($priorityFunctions !== '') {
     $counter++;
@@ -69,6 +64,7 @@ if (isset($_POST['calculatorInput']) && $_POST['calculatorInput'] !== '') {
     eval('$priorityTotal = ('.$priorityFunctions.');');
     echo ('<p><strong>'.$counter.'.</strong> <i>('.$priorityFunctions.')</i> = <strong>'.$priorityTotal.'</strong></p>');
     $calculatorInput = str_replace($priorityFunctions, $priorityTotal, $calculatorInput); }
+
 // / The following code parses upper-level equations.
 if ($calculatorInput !== '') {
   $counter++;
