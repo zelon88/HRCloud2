c7bfcd94d8be0a2e807125a48a0ba3dbc6366c1e
zelon88 <zelon88@gmail.com>
2017-09-13 23:20:19 -0400

    v1.7.7.6 - Update "Tips".
    -v1.7.7.6.
    -Added a few Tips to the $Tips array.

--- Resources/tips.php
+++ Resources/tips.php
@@ -2,7 +2,7 @@
 $Tips = array(
 'You can search for files in your "Drive" by using the <img src="Resources/searchsmall.png"></img> button!',
 'The logs on your "Logs" page are specific to your user only.',
-'You can launch any HRC2 App from the "Apps" page in a new window by clicking the "<img src="Resources/newwindow.png"></img>" button!',
+'You can launch any HRC2 App from the "Apps" page in a new window by clicking the <img src="Resources/newwindow.png"></img> button!',
 'When you change settings on the "Settings" page, you\'re only making changes for yourself, unless you\'re an administrator',
 'You can convert a PDF file to a text or Office document from your "Drive" by clicking on the <img src="Resources/makepdf.png"></img> button!',
 'The Pell App in your "Apps" page can open, modify, and save Microsoft Office documents!',
@@ -10,4 +10,12 @@ $Tips = array(
 'The official <a href="https://github.com/zelon88/HRCloud2/wiki">HRCloud2 Wiki</a> contains the official User Manual as well as other useful information about your Cloud!',
 'Every attribute (name, size, last modified, ect...) on your "Drive" page is sortable by clicking on the table\'s header.',
 'You can extract audio from videos from your "Drive" by clicking the <img src="Resources/convert.png"></img> button!',
-'You can copy and paste files from one folder to another in your "Drive" by clicking the <img src="Resources/clipboard.png"></img> button!');
+'You can copy and paste files from one folder to another in your "Drive" by clicking the <img src="Resources/clipboard.png"></img> button!',
+'Any "Drive" operation can be performed on multiple items at once by selecting multiple files to save time.',
+'If your admin has selected virus scanning, your files are automatically scanned with ClamAV Open-Source Anti-Virus!',
+'When you share files, you can right-click on the file on the "Shared Files" page and copy/paste it\'s location anywhere! Perfect for social media, or sharing music or movies with friends.',
+'HRAI can do math problems, convert files, tell you the time, give you server stats and specs, and more, but she\'s a blank-slate. Create your own coreCommands, plugins, or re-write her completely!',
+'Stick with Open-Source software, apps, and plugins and stick it to the man with us!',
+'Zelon88 created HRCloud2. If you get stuck <a href="mailto:zelon88@gmail.com?Subject=HRC2%20Help" target="_top">send him an email</a>!',
+'The Calculator App can handle mathematic or plain speech commands like "What is 2 plus 2 times 2" or "2+2(2-2x22)-2" with better precision than Google. If the answer is too long it returns scientific notation.',
+'To see what HRAI\'s thinking, click the "Console" button in the top right corner of your HRAI window.');
