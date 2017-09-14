678936878f6619681a35c0733bfc9830ace65501
zelon88 <zelon88@gmail.com>
2017-09-13 22:52:21 -0400

    v1.7.7.6 - Added "Tips" & setting to disable them.
    -v1.7.7.6.
    -Added "Tips" to the top of most pages.
    -Users can select an option to disable Tips via their "Settings" page.

---
+++ Resources/tips.php
@@ -0,0 +1,13 @@
+<?php
+$Tips = array(
+'You can search for files in your "Drive" by using the <img src="Resources/searchsmall.png"></img> button!',
+'The logs on your "Logs" page are specific to your user only.',
+'You can launch any HRC2 App from the "Apps" page in a new window by clicking the "<img src="Resources/newwindow.png"></img>" button!',
+'When you change settings on the "Settings" page, you\'re only making changes for yourself, unless you\'re an administrator',
+'You can convert a PDF file to a text or Office document from your "Drive" by clicking on the <img src="Resources/makepdf.png"></img> button!',
+'The Pell App in your "Apps" page can open, modify, and save Microsoft Office documents!',
+'The Grabber App on your "Apps" page can download a file from a URL straight to your Cloud Drive! Perfect for saving mobile data on-the-go.',
+'The official <a href="https://github.com/zelon88/HRCloud2/wiki">HRCloud2 Wiki</a> contains the official User Manual as well as other useful information about your Cloud!',
+'Every attribute (name, size, last modified, ect...) on your "Drive" page is sortable by clicking on the table\'s header.',
+'You can extract audio from videos from your "Drive" by clicking the <img src="Resources/convert.png"></img> button!',
+'You can copy and paste files from one folder to another in your "Drive" by clicking the <img src="Resources/clipboard.png"></img> button!');
