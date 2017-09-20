da17fc0db1b9283b8a417aefddd18d362a1c3d06
zelon88 <zelon88@gmail.com>
2017-09-19 22:42:58 -0400

    UberGallery App to v3.0 - Initial commit.
    -UberGallery App to v3.0.
    -Working on adding a Gallery App for viewing photos in your Cloud Drive.

---
+++ Applications/UberGallery/resources/themes/uber-responsive/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <title>UberGallery</title>
+
+    <link rel="shortcut icon" href="<?php echo THEMEPATH; ?>/img/favicon.png" />
+
+    <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/css/bootstrap-responsive.min.css" />
+    <?php echo $gallery->getColorboxStyles(1); ?>
+
+    <script type="text/javascript" src="resources/jquery-2.1.4.js"></script>
+    <script type="text/javascript" src="<?php echo THEMEPATH; ?>/js/bootstrap.min.js"></script>
+    <?php echo $gallery->getColorboxScripts(); ?>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="navbar navbar-inverse">
+            <div class="navbar-inner">
+                <div class="container">
+                    <div class="brand">UberGallery</div>
+                </div>
+            </div>
+        </div>
+
+        <?php if($gallery->getSystemMessages()): ?>
+            <?php foreach($gallery->getSystemMessages() as $message): ?>
+                <div class="alert alert-<?php echo $message['type']; ?>">
+                    <a class="close" data-dismiss="alert">×</a>
+                    <?php echo $message['text']; ?>
+                </div>
+            <?php endforeach; ?>
+        <?php endif; ?>
+
+        <!-- Start UberGallery v<?php echo UberGallery::VERSION; ?> - Copyright (c) <?php echo date('Y'); ?> Chris Kankiewicz (http://www.ChrisKankiewicz.com) -->
+        <?php if (!empty($galleryArray) && $galleryArray['stats']['total_images'] > 0): ?>
+            <ul class="gallery-wrapper thumbnails">
+                <?php foreach ($galleryArray['images'] as $image): ?>
+                    <li class="">
+                        <a href="<?php echo str_replace('/var/www/html/', '', html_entity_decode($image['file_path'])); ?>" title="<?php echo $image['file_title']; ?>" class="thumbnail" rel="colorbox">
+                            <img src="<?php echo $image['thumb_path']; ?>" alt="<?php echo $image['file_title']; ?>" />
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+        <!-- End UberGallery - Distributed under the MIT license: http://www.opensource.org/licenses/mit-license.php -->
+
+        <hr/>
+
+
+        <?php if ($galleryArray['stats']['total_pages'] > 1): ?>
+
+            <div class="pagination pagination-centered">
+                <ul>
+                    <?php foreach ($galleryArray['paginator'] as $item): ?>
+
+                        <?php
+
+                            switch ($item['class']) {
+
+                                case 'title':
+                                    $class = 'disabled';
+                                    break;
+
+                                case 'inactive':
+                                    $class = 'disabled';
+                                    break;
+
+                                case 'current':
+                                    $class = 'active';
+                                    break;
+
+                                case 'active':
+                                    $class = NULL;
+                                    break;
+
+                            }
+
+                        ?>
+
+                        <li class="<?php echo $class; ?>">
+                            <a href="<?php echo str_replace('/var/www/html/', '', empty($item['href']) ? '#' : $item['href']); ?>"><?php echo $item['text']; ?></a>
+                        </li>
+
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endif; ?>
+
+    </div>
+
+</body>
+
+<!-- Page template by, Chris Kankiewicz <http://www.chriskankiewicz.com> -->
+
+</html>
