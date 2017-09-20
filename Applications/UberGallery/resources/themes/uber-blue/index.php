702c2df0c0998ecd5f2671c4d2e94ba809d70763
zelon88 <zelon88@gmail.com>
2017-09-19 22:38:15 -0400

    UberGallery App to v3.0 - Initial commit.
    -UberGallery App to v3.0.
    -Working on adding a Gallery App for viewing photos in your Cloud Drive.

---
+++ Applications/UberGallery/resources/themes/uber-blue/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>UberGallery</title>
+    <link rel="shortcut icon" href="<?php echo THEMEPATH; ?>/images/favicon.png" />
+
+    <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/rebase-min.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/style.css" />
+    <?php echo $gallery->getColorboxStyles(5); ?>
+
+    <script type="text/javascript" src="resources/jquery-2.1.4.js"></script>
+    <?php echo $gallery->getColorboxScripts(); ?>
+
+</head>
+<body>
+
+<!-- Start UberGallery v<?php echo UberGallery::VERSION; ?> - Copyright (c) <?php echo date('Y'); ?> Chris Kankiewicz (http://www.ChrisKankiewicz.com) -->
+<div id="galleryWrapper">
+    <h1>UberGallery</h1>
+    <div class="line"></div>
+
+    <?php if($gallery->getSystemMessages()): ?>
+        <ul id="systemMessages">
+            <?php foreach($gallery->getSystemMessages() as $message): ?>
+                <li class="<?php echo $message['type']; ?>">
+                    <?php echo $message['text']; ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+
+    <div id="galleryListWrapper">
+        <?php if (!empty($galleryArray) && $galleryArray['stats']['total_images'] > 0): ?>
+            <ul id="galleryList" class="clearfix">
+                <?php foreach ($galleryArray['images'] as $image): ?>
+                    <li><a href="<?php echo str_replace('/var/www/html/', '', html_entity_decode($image['file_path'])); ?>" title="<?php echo $image['file_title']; ?>" rel="colorbox"><img src="<?php echo $image['thumb_path']; ?>" alt="<?php echo $image['file_title']; ?>"/></a></li>
+                <?php endforeach; ?>
+            </ul>
+        <?php endif; ?>
+    </div>
+
+    <div class="line"></div>
+    <div id="galleryFooter" class="clearfix">
+
+        <?php if ($galleryArray['stats']['total_pages'] > 1): ?>
+        <ul id="galleryPagination">
+
+            <?php foreach ($galleryArray['paginator'] as $item): ?>
+
+                <li class="<?php echo $item['class']; ?>">
+                    <?php if (!empty($item['href'])): ?>
+                        <a href="<?php echo str_replace('/var/www/html/', '', $item['href']); ?>"><?php echo $item['text']; ?></a>
+                    <?php else: ?><?php echo $item['text']; ?><?php endif; ?>
+                </li>
+
+            <?php endforeach; ?>
+
+        </ul>
+        <?php endif; ?>
+
+    </div>
+</div>
+<!-- End UberGallery - Distributed under the MIT license: http://www.opensource.org/licenses/mit-license.php -->
+
+</body>
+</html>
