1e2f409542e593c7d8d2d5bd654cca3a00c43eb3
zelon88 <zelon88@gmail.com>
2017-09-21 00:20:23 -0400

    Fixed typo.

--- settingsCore.php
+++ settingsCore.php
@@ -326,7 +326,7 @@ if ($ShowTips !== '1') {
   <select id="GenClientCPU" name="GenClientCPU" style="float:left; padding-left:30px; width:30%;">
   <option value="">Select your CPU</option>
   <option value="x64">64-Bit (x64)</option>
-  <option value="ia32">32-Bit (x32)</option>
+  <option value="ia32">32-Bit (x86)</option>
   <option value="armv71">ARMv71</option>
   </select>
   <select id="GenClientHomepage" name="GenClientHomepage" style="float:left; padding-left:30px; width:30%;">
