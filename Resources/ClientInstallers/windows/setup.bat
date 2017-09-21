8318757ffc03bbfca129a329147205c9b4727c9a
zelon88 <zelon88@gmail.com>
2017-09-20 23:45:31 -0400

    v1.8 - Added Client App generator.
    -v1.8.
    -Added a generator to HRCloud2 for building Desktop Client Apps.
    -Currently supports Windows, Linux, and OSX.
    -Supports 32-bit, 64-bit, and ARMv71 CPU's.
    -Creates a new Client App from the Settings page.

---
+++ Resources/ClientInstallers/windows/setup.bat
@@ -0,0 +1,26 @@
+ECHO OFF
+CLS
+:MENU
+ECHO v1.0
+ECHO ...............................................
+ECHO WELCOME TO THE HRCLOUD2-CLIENT INSTALLER FOR WINDOWS!
+ECHO PRESS  Y  TO INSTALL HRCLOUD2. PRESS  N  TO EXIT
+ECHO ...............................................
+ECHO.
+ECHO Y - Install HRCloud2-Client
+ECHO N - EXIT
+ECHO.
+SET /P M=Type Y or N, then press ENTER:
+IF %M%==Y GOTO INSTALL
+IF %M%==N GOTO EOF
+
+:INSTALL
+mkdir "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\HRCloud2"
+xcopy "" "C:\Program Files\HRCloud2" /s 
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\HRCloud2-Client.lnk');$s.TargetPath='C:\Program Files\HRCloud2\HRCloud2-Client.exe';$s.Save()"
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\HRCloud2-Client.lnk');$s.TargetPath='C:\Program Files\HRCloud2\HRCloud2-Client.exe';$s.Save()"
+PAUSE
+EXIT /B
+
+GOTO MENU
+EXIT /B
