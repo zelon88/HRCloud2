a67525a74d2313dd91ab59b2817c6212c9677c51
zelon88 <zelon88@gmail.com>
2017-10-20 00:26:10 -0400

    v1.8.8 - Version adjustment.
    -Adjust version to test update install method.

--- compatibilityCore.php
+++ compatibilityCore.php
@@ -2,8 +2,8 @@
 
 /*
 HRCLOUD2 VERSION INFORMATION
-THIS VERSION : v1.8.7.7
-WRITTEN ON : 10/12/2017
+THIS VERSION : v1.8.8
+WRITTEN ON : 10/20/2017
 */
 
 // / -----------------------------------------------------------------------------------
