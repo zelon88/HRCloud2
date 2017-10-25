ff1c319043d0e095ad4351cbf01eb96097a54641
zelon88 <zelon88@gmail.com>
2017-10-25 15:29:51 -0400

    PHP-AV App to v3.0. Defs to v4.5.
    -PHP-AV App to v3.0.
    -Defs to v4.5.
    -Refactored the entire application.
    -Improved performance, readability, usability.
    -Tweaked styling a little bit.

---
+++ Applications/PHP-AV/Resources/common.js
@@ -0,0 +1,10 @@
+    function Clear() {    
+      document.getElementById("AVScanTarget").value= ""; }
+    function toggle_visibility(id) {
+      var e = document.getElementById(id);
+      if(e.style.display == 'block')
+         e.style.display = 'none';
+      else
+         e.style.display = 'block'; }
+    function goBack() {
+      window.history.back(); }
