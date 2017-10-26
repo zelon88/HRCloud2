5e9875a6061bc184dd35ae2f94c6e1b9cbc1f2d9
zelon88 <zelon88@gmail.com>
2017-10-26 00:49:10 -0400

    v1.9 - Updated header. PHP-AV App to v3.1.
    -Added three horizontal bars to header to replace "Settings"
    -Added a "Back" button to the header.
    -PHP-AV App to v3.1.
    -Added PHP-AV options for Memory Limit and Chunk Size in the options menu.
    -Added "Notes" section to PHP-AV to explain generated results.

--- header.php
+++ header.php
@@ -3,12 +3,13 @@
 <div id="nav" align="center">
     <div class="nav">
       <ul>
+        <li class="Back"><a alt="Back" title="Back" href="javascript:history.go(-1)"> <b>←</b></a></li>
         <li class="Cloud"><a href="index1.php">Cloud</a></li>
         <li class="Apps"><a href="appLauncher.php">Apps</a></li>
         <li class="Drive"><a href="index2.php">Drive</a></li>
-        <li class="Settings"><a href="settings.php"> Settings</a></li>
         <li class="Logs"><a href="logs.php">Logs</a></li>
         <li class="Help"><a href="help.php">Help</a></li>
+        <li class="Settings"><a href="settings.php"> <b>≡</b></a></li>
       </ul>
     </div>
 </div>
