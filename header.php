1a1b0933ad3991e5204fbb872a414865ad3cd2e5
zelon88 <zelon88@gmail.com>
2017-10-30 21:04:54 -0400

    v1.9.1-Add alt text to headers, Update UberGallery
    -v1.9.1.
    -UberGallery App to v3.1.
    -Fixed bugs with relative paths in UberGallery App.
    -Added alt-text and titles to header.php elements.

--- header.php
+++ header.php
@@ -4,12 +4,12 @@
     <div class="nav">
       <ul>
         <li class="Back"><a alt="Back" title="Back" href="javascript:history.go(-1)"> <b>←</b></a></li>
-        <li class="Cloud"><a href="index1.php">Cloud</a></li>
-        <li class="Apps"><a href="appLauncher.php">Apps</a></li>
-        <li class="Drive"><a href="index2.php">Drive</a></li>
-        <li class="Logs"><a href="logs.php">Logs</a></li>
-        <li class="Help"><a href="help.php">Help</a></li>
-        <li class="Settings"><a href="settings.php"> <b>≡</b></a></li>
+        <li class="Cloud"><a alt="Cloud Home" title="Cloud Home" href="index1.php">Cloud</a></li>
+        <li class="Apps"><a alt="App Launcher" title="App Launcher" href="appLauncher.php">Apps</a></li>
+        <li class="Drive"><a alt="Cloud Drive" title="Cloud Drive" href="index2.php">Drive</a></li>
+        <li class="Logs"><a alt="Logs" title="Logs" href="logs.php">Logs</a></li>
+        <li class="Help"><a alt="Help" title="Help" href="help.php">Help</a></li>
+        <li class="Settings"><a alt="Settings" title="Settings" href="settings.php"> <b>≡</b></a></li>
       </ul>
     </div>
 </div>
