b8ec7f8e47cbfd5fbf61d9c82d909c89baa7d079
zelon88 <zelon88@gmail.com>
2017-11-24 21:23:57 -0500

    v2.1.6 - Fixed minor display bugs.
    -v2.1.6.
    -Logs no longer show Favorites.php.
    -Logs page now shows correct re-load button.

--- compatibilityCore.php
+++ compatibilityCore.php
@@ -2,8 +2,8 @@
 
 /*
 HRCLOUD2 VERSION INFORMATION
-THIS VERSION : v2.1.5
-WRITTEN ON : 11/20/2017
+THIS VERSION : v2.1.6
+WRITTEN ON : 11/24/2017
 */
 
 // / -----------------------------------------------------------------------------------
