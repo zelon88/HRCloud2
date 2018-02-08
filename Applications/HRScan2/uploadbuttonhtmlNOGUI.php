0da5f67be9e4d77b72be6c2df75430e25d31d420
zelon88 <zelon88@gmail.com>
2018-02-07 22:08:23 -0500

    v2.3 - HRAI text-to-speech/refactor. Nicknames.
    -v2.3.
    -HRAI now talks! 
    -Thanks to mikolalysenko/mespeak for the sweet text-to-speech Javascript library!
    -HRAI now uses the nickname specified in the user config file.
    -HRC2 now sets default values for timezone and nickname settings if none are set (old config files).
    -Clean up a lot of code, comments, and misc stuff.
    -HRAI to v5.3.
    -Consolidates and cleans the coreCommands outputs to mostly generate $output that can be spoken by the text-to-speech engine.
    -Modifies just about every core command to implement the new unified $output template.
    -Adjust appIndex GUI spacing.

--- Applications/HRScan2/uploadbuttonhtmlNOGUI.php
+++ Applications/HRScan2/uploadbuttonhtmlNOGUI.php
@@ -11,7 +11,7 @@
  <div id="HRAIScanOptions0" name="HRAIScanOptions0" style="display: block;">
 <?php
 if ($user_ID == '0') {
-echo nl2br('Unfortunately I cannot scan files for you at this time. Try logging in. '."\r".' I appologize, Commander.'); }
+echo nl2br('Unfortunately I cannot scan files for you at this time. Try logging in. '."\r"); }
 if ($user_ID > '0') {  ?>
 <p><a id="nextbutton" name="nextbutton" href="#top" target="HRAIMini" style="max-width:100px; border-style: solid; border-color: MidnightBlue;" onclick="toggle_visibility('HRAIScanSubmit'); toggle_visibility('HRAIScanOptions1'); toggle_visibility('HRAIScanOptions0');">Next &#x2192</a></p>
   <p>Please enter a Cloud directory/filename to scan:</p>
