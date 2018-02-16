eeee27a393119830764520fa5b4a57f834649434
zelon88 <zelon88@gmail.com>
2018-02-15 23:58:22 -0500

    v2.2.4 - HRAI to v5.5 Fix Audio setting, paths.
    -v2.2.4.
    -HRAI to v5.5.
    -Fix bugs with HRAI using admin audio settings instead of user audio settings.
    -Fix issue #21, HRAIMiniGui input box no longer disappears after user input.
    -Improve HRAI input sanitization.
    -Improve HRAIMiniGui and HRAI core path declarations to improve cross-platform support.
    -Remove config.php and securityCore.php calls in HRAI core, improving performance and resolving bugs with configuration resets.
    -Remove HRAIMiniGui auto input population as it was awkward and poorly implemented.

--- Applications/HRAIMiniGui.php
+++ Applications/HRAIMiniGui.php
@@ -1,30 +1,36 @@
 
 <link rel="stylesheet" type="text/css" href="style.css">
 <script type="text/javascript" src="/HRProprietary/HRCloud2/Applications/jquery-3.1.0.min.js"></script>
+<script type="text/javascript" src="/HRProprietary/HRCloud2/Applications/meSpeak/mespeak.js"></script>
 
   <?php 
-include('/var/www/html/HRProprietary/HRCloud2/config.php');
+if (!file_exists('../commonCore.php')) {
+  echo nl2br('ERROR!!! HRC2HMG35, Cannot process the HRCloud2 Common Core file (commonCore.php).'."\n"); 
+  die (); }
+else {
+  require_once ('../commonCore.php'); }
 // / This file serves as the mini GUI for the HRAI module of HRCloud2. It was not intended to be used outside of 
 // / HRCloud2, although this file provides an example of a simple HTML API call to an HRAI node.
 if (!isset($noMINICore)) {
   $noMINICore = '0'; }
 
-$includeMINIIframerURL = 'core.php#end';  
-if (!isset($includeMINIIframer)) {
-  $includeMINIIframer = '1';
-  $includeMINIIframerURL = 'HRAI/core.php#end'; }
-
 if ($noMINICore == '0') {
-  require($InstLoc.'/Applications/HRAI/core.php'); }
-if ($includeMINIIframer == '1') { ?>
+  require($InstLoc.'/Applications/HRAI/core.php'); } ?>
   <div align="center" id='HRAIButtons2' name='HRAIButtons2'>
   <hr />
-  <form action="<?php echo $includeMINIIframerURL; ?>" id="Corefile Input" method="post">
+  <form action="HRAIMiniGui.php" id="Corefile Input" method="post">
   <input type="hidden" name="HRAIMiniGUIPost" value="1">
   <input type="hidden" name="display_name" value="<?php echo $display_name;?>">
   <?php if (!isset($input)) {
     $input = ''; } ?>
-  <input type="text" name="input" id="input"  value="<?php echo $input; ?>">
+  <input type="text" name="input" id="input"  value="">
   <input id='submitHRAI' type="submit" value="Hello HRAI"></form>
   </div>
+</div><?php
+if ($HRAIAudio == '1') { ?>
+  <script type="text/javascript">
+  meSpeak.speak('<?php echo $cleanOutput; ?>');
+  meSpeak.loadConfig('Applications/meSpeak/mespeak_config.json');
+  meSpeak.loadVoice('Applications/meSpeak/voices/en/en-us.json');
+  </script>
 <?php } ?>
