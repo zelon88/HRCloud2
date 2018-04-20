bef60a30f5f932738737ae849114f87997415ae2
zelon88 <zelon88@gmail.com>
2018-04-20 01:36:29 -0400

    v2.5.1 - Add csv to API. Update Windows client.
    -v2.5.1.
    -Add .csv conversion and support for csv files to the API, but not the GUI since the conversion support is spotty.
    -Update the setup.bat file in the Windows Client installer package to v1.1 to more reliably copy files to the proper location.

--- Resources/ClientInstallers/windows/setup.bat
+++ Resources/ClientInstallers/windows/setup.bat
@@ -1,11 +1,14 @@
 ECHO OFF
 CLS
 :MENU
-ECHO v1.0
-ECHO ...............................................
+ECHO v1.1
+ECHO.
+ECHO .....................................................
 ECHO WELCOME TO THE HRCLOUD2-CLIENT INSTALLER FOR WINDOWS!
+ECHO .....................................................
+ECHO            --RUN AS ADMINISTRATOR--
 ECHO PRESS  Y  TO INSTALL HRCLOUD2. PRESS  N  TO EXIT
-ECHO ...............................................
+ECHO .....................................................
 ECHO.
 ECHO Y - Install HRCloud2-Client
 ECHO N - EXIT
@@ -17,10 +20,11 @@ IF %M%==N GOTO END
 IF %M%==n GOTO END
 
 :INSTALL
+SET currentPath=%~dp0
 ECHO CREATING DIRECTORUIES!
 mkdir "C:\Program Files\HRCloud2"
 ECHO COPYING FILES!
-xcopy *.* "C:\Program Files\HRCloud2" /s 
+xcopy /S "%currentPath:~0, -1%" "C:\Program Files\HRCloud2" 
 ECHO CREATING SHORTCUTS!
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\HRCloud2-Client.lnk');$s.TargetPath='C:\Program Files\HRCloud2\HRCloud2-Client.exe';$s.Save()"
 powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\HRCloud2-Client.lnk');$s.TargetPath='C:\Program Files\HRCloud2\HRCloud2-Client.exe';$s.Save()"
