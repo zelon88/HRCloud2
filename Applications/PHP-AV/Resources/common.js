b379699df720762e5283bb106a4efa049f42cace
zelon88 <zelon88@gmail.com>
2018-07-02 00:22:13 -0400

    v2.7 - PHP-AV App to v3.2.
    -v2.7.
    -Clean up some redundant and WET code.
    -PHP-AV App to v3.2.
    -Fix tons of bugs, improve performance and reliability.
    -Added a real-time console that shows details of the scan-in-progress.
    -Improve hashing, scanning, detection.
    -Added a link to download a report of the scan when complete.

--- Applications/PHP-AV/Resources/common.js
+++ Applications/PHP-AV/Resources/common.js
@@ -1,10 +1,16 @@
-    function Clear() {    
-      document.getElementById("AVScanTarget").value= ""; }
-    function toggle_visibility(id) {
-      var e = document.getElementById(id);
-      if(e.style.display == 'block')
-         e.style.display = 'none';
-      else
-         e.style.display = 'block'; }
-    function goBack() {
-      window.history.back(); }
+function Clear() {    
+  document.getElementById("AVScanTarget").value= ""; }
+
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'block')
+    e.style.display = 'none';
+  else
+    e.style.display = 'block'; }
+
+function goBack() {
+  window.history.back(); }
+
+function refreshIframe() {
+  var frame = document.getElementById("consoleFrame");
+  frame.src = frame.src; }
