827059d9d0c7ba0ed2b2723c37f6d58aaff6e0f4
zelon88 <zelon88@gmail.com>
2018-07-08 00:29:50 -0400

    v2.8 - Update lots of apps!
    -Added a changelog to the repo to keep track of commits, notes, and other brainstorming stuff.
    -PHP-AV App to v3.3.
    -PHP-AV Scan targets now work properly.
    -PHP-AV header text now disappears when a scan is started, making more room for a pleasant console experience.
    -Improve cross platform support with relative paths throughout a lot of apps.
    -ServMonitor App to v3.1.
    -ServMonitor labels have been corrected, USB Devices -> Devices, PCI Devices -> Uptime.
    -Notes App to v
    -Contacts App to v2.3.
    -Grabber App to v1.9.
    -Calendar App to v0.6.
    -Bookmarks App to v1.6.
    -Calculator App to v1.6.
    -Pell to v3.4.
    -HRAI to v5.8.
    -Remove HRAI's ability to authenticate users as it is not needed.
    -Remove old experimental alpha code for natural language parsing.
    -Remove the sesLog creation and verification code.
    -Remove a lot of files that are no longer needed.
    -Improve HRAI performance by a considerable amount.
    -Reduce HRAI disk, memory, and CPU utilization, improve privacy & security.
    -Updated the HRConvert2 module of HRAI to use an installed version of HRConvert2 on the local server.
    -Added cleanup code to compatCore.php to deal with recent changes in HRAI.
    -Enabled the "Check Perms" functionality in settingsCore.php and added the UI elements to finally use it.

--- Applications/Notes/Notes.php
+++ Applications/Notes/Notes.php
@@ -4,7 +4,7 @@
 /*//
 HRCLOUD2-PLUGIN-START
 App Name: Notes
-App Version: 2.0 (7-23-2017 13:30)
+App Version: v2.1 (7-7-2018 00:00)
 App License: GPLv3
 App Author: zelon88
 App Description: A simple HRCloud2 App for creating, viewing, and managing notes and to-do lists!
@@ -23,19 +23,19 @@ HRCLOUD2-PLUGIN-END
  
 // / The follwoing code checks if the sanitizeCore.php file exists and 
 // / terminates if it does not.
-if (!file_exists('/var/www/html/HRProprietary/HRCloud2/sanitizeCore.php')) {
+if (!file_exists('../../sanitizeCore.php')) {
   echo nl2br('</head><body>ERROR!!! HRC2AL10, Cannot process the HRCloud2 Sanitization Core file (sanitizeCore.php)!'."\n".'</body></html>'); 
   die (); }
 else {
-  require_once ('/var/www/html/HRProprietary/HRCloud2/sanitizeCore.php'); }
+  require_once ('../../sanitizeCore.php'); }
 
 // / The follwoing code checks if the commonCore.php file exists and 
 // / terminates if it does not.
-if (!file_exists('/var/www/html/HRProprietary/HRCloud2/commonCore.php')) {
+if (!file_exists('../../commonCore.php')) {
   echo nl2br('</head><body>ERROR!!! HRC2AL18, Cannot process the HRCloud2 Common Core file (commonCore.php)!'."\n".'</body></html>'); 
   die (); }
 else {
-  require_once ('/var/www/html/HRProprietary/HRCloud2/commonCore.php'); }
+  require_once ('../../commonCore.php'); }
 
 // / The following code ensures the Notes directory exists and creates it if it does not.
 $SaltHash = hash('ripemd160',$Date.$Salts.$UserIDRAW);
