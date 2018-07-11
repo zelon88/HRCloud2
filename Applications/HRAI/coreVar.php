a53fbdf92dcbcb5abe57ece3e8438c2af06f0060
zelon88 <zelon88@gmail.com>
2018-07-11 00:10:41 -0400

    v2.8.3 - Continue refactor. Add stylesheet control
    -v2.8.3.
    -Fix bug with required directory handler.
    -Continue minimizing cores.
    -Fix index generator.
    -Consolidate stylesheet generators into commonCore.php instead of spreading the both of them out into guiCore and commonCore.
    -Add minStyles, maxStyles, and allStyles variables for Apps and plugins. Specify before loading commonCore.php for them to take effect.
    -ServMonitor to v3.2, add noStyles variable.
    -HRAI to v5.9, add noStyles variable.
    -UberGallery to v3.4, add noStyles variable.
    -ServStat to v1.2, add noStyles variable.
    -PHP-AV App to v3.4, add minStyles variable.
    -Notes App to v2.2, add maxStyles variable.
    -Contacs App to v2.4, add maxStyles variable.
    -Bookmarks App to v1.7, add maxStyles variable.

--- Applications/HRAI/coreVar.php
+++ Applications/HRAI/coreVar.php
@@ -1,5 +1,5 @@
 <?php
-$HRAI_VERSION = 'v5.8';
+$HRAI_VERSION = 'v5.9';
 $InstLoc = '/var/www/html/HRProprietary/HRCloud2';
 $HRAIMiniGUIFile = $InstLoc.'/Applications/HRAIMiniGui.php';
 $HRC2SecurityCoreFile = $InstLoc.'/securityCore.php';
@@ -19,14 +19,4 @@ $HRAIWordgap = '50';
 $HRAISpeed = '175';
 $HRAIPitch = '0';
 $HRAIVoiceType = '';
-// / $CallForHelp   0 = Automatically call for help.  1 = Don't automatically call for help.
-$CallForHelp = 0;
-// / $getServBusy automatically assumes that the server is idle. This avoids a potential loop in core.php
-// / where the server could potentially keep calling for help. For good measure, always match this var with
-// / $CallForHelp, just-in-case. 
-$getServBusy = 0;
-
-
-
-
-// / Everything and anything below this line is machine created code.
+$noStyles = 1;
