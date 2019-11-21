8a2a693852d1461f9e89e3ed9bcdb813de0ee570
Justin Grimes <zelon88@gmail.com>
2019-11-20 20:01:06 -0500

    v3.2 - Fix bug with wordpress installation package formatted incorrectly.
    -v3.2.
    -Issue# 26. https://github.com/zelon88/HRCloud2/issues/26
    -The Wordpress installation package was misformatted and as such HRCloud2 was unable to install it.
    -Updated included Wordpress package to latest v5.3.

--- compatibilityCore.php
+++ compatibilityCore.php
@@ -2,8 +2,8 @@
 
 /*
 HRCLOUD2 VERSION INFORMATION
-THIS VERSION : v3.1.9
-WRITTEN ON : 10/22/2019
+THIS VERSION : v3.2
+WRITTEN ON : 11/20/2019
 */
 
 // / -----------------------------------------------------------------------------------
