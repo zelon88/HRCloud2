148cbd6b5d6e71191c6dc15c73dcba7dc6ab4d3e
Justin Grimes <zelon88@gmail.com>
2021-04-13 22:16:07 -0400

    v3.5.3 - Calculator to v1.7, ServStat to v1.3, Grabber to v2.0, PHP-AV to v4.1, Weather to v0.7, Calendar to v0.7.
    -Calculator to v1.7.
      -Fix syntax errors on PHP 7.4.3.
    -ServStat to v1.3.
      -Make app admin-only until I can make the server.xml file user-specific.
      -Fix typo in app declaration block.
    -Grabber to v2.0.
      -Refactor a lot of old and embarrassing code.
      -Remove a lot of redundant checks (does_url_exist being called more than once).
      -Add ability to infer the file extension from the URL. 
      -Users no longer have to type the filename into the filename field. Grabber figures out the file extension and names the file appropriately.
      -Fix syntax errors on PHP 7.4.3.
    -PHP-AV to v4.1.
      -Fix improperly nested code block.
      -Fix syntax errors on PHP 7.4.3.
    -Weather to v0.7.
      -Fix typo in $key declaration.
    -Calendar to v0.7.
      -Add noStyles variable to prevent default stylesheets from loading.
      -Fix syntax errors on PHP 7.4.3.
    -Fix the way stylesheet declarations are processed in sanitizeCore.php.

--- Applications/Weather/Weather.php
+++ Applications/Weather/Weather.php
@@ -3,7 +3,7 @@
 /*//
 HRCLOUD2-PLUGIN-START
 App Name: Weather
-App Version: v0.6 (4-8-2021 00:00)
+App Version: v0.7 (4-13-2021 00:00)
 App License: GPLv3
 App Author: lannes & zelon88
 App Description: A simple HRCloud2 Weather app.
